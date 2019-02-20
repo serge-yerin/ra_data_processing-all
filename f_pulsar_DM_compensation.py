ec6e96428c9d04e4d69f6c572a5c2327ea5d2914
Serge Yerin <43250974+serge-yerin@users.noreply.github.com>
2019-02-20 22:39:07 +0200

    Create f_pulsar_DM_compensation.py

---
+++ f_pulsar_DM_compensation.py
@@ -0,0 +1,33 @@
+'''
+'''
+
+import numpy as np
+import math
+from f_plot_formats import plot1D
+
+
+def DM_compensation(matrix_0, freq_points, fmin, fmax, df, TimeRes, pulsarPeriod, DM0, filename, save_intermediate_data, customDPI):
+    '''
+    Plots 1D plots of variable
+    '''
+    dt = np.zeros((freq_points), dtype = 'float')                # time of shifting channel to dedisperse
+    for i in range (freq_points):
+        dt[i] = (DM0 / 2.410331) * ((10000./math.pow((fmin + df * i), 2)) - (10000./math.pow(fmax, 2))) 
+    shiftPar = np.zeros((freq_points), dtype = 'int')            # number of steps to shift each row
+    for i in range (freq_points):
+        if filename[0:3] == 'ADR':
+            shiftPar[i] = -1 * round((1/2.) * pulsarPeriod * (dt[i] / pulsarPeriod - math.floor(dt[i] / pulsarPeriod)) / TimeRes)
+        if filename[0:3] == 'DSP':
+            shiftPar[i] = -1 * round(pulsarPeriod * (dt[i] / pulsarPeriod - math.floor(dt[i] / pulsarPeriod)) / TimeRes)
+        matrix_0[i] = np.roll(matrix_0[i], shiftPar[i])
+    
+    if save_intermediate_data == 1:
+    
+        ShiftParTXT = open(filename + '_results/Shift parameter.txt', "w")
+        for i in range(freq_points):
+            ShiftParTXT.write(str(fmin + df * i)+'   '+str(shiftPar[i])+' \n' )
+        ShiftParTXT.close()
+        
+        plot1D(shiftPar, filename + '_results/01.2 - Shift parameter.png', 'Shift parameter', 'Shift parameter', 'Shift parameter', 'Frequency channel number', customDPI)
+    
+    return matrix_0
