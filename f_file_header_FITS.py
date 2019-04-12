d2e3be884886ca93ab50b743271927fa48ba7cde
Serge Yerin <serge-yerin@users.noreply.github.com>
2019-04-13 00:14:59 +0300

    FITS improved frequency axis calculation

--- f_file_header_FITS.py
+++ f_file_header_FITS.py
@@ -54,116 +54,86 @@ def FileHeaderReaderFITS(foldpath, filename):
     file = open(foldpath + filename, 'rb')
     hdul = fits.open(foldpath + filename)
     #hdul.info()
+    print ('  File: ', filename)
+    
+    
+    num_of_groups = 8
+    num_of_fields = np.zeros(num_of_groups, dtype = 'int')
+    num_of_column = np.zeros(num_of_groups, dtype = 'int')
+    for i in range (num_of_groups):
+        num_of_fields[i] = int(hdul.info(0)[i][4])
+ 
+    for group in range (1, num_of_groups):
+        num_of_column[group] = int(len(hdul[group].columns))
+        
+    
+    
+    
+    print('\n * Header # 0   PRIMARY \n')
+    for i in range (len(hdul[0].header)):
+        print (' %2i' % i, ' %50s: ' % hdul[0].header.comments[i], hdul[0].header[i])
+        
+        
+    
+    for group in range (1, num_of_groups):
+        print ('\n ********************************************************************************** \n')
+        print(' * Header # ', group,'   ', hdul.info(0)[group][1],'\n')
+        for i in range (num_of_column[group]):
+            
+            hdr = hdul[0].header
+            cols = hdul[1].columns
+            data = hdul[1].data
+            
+            
+            if hdul[group].data.field(i).shape == (1,):  
+                temp_string = hdul[group].data.field(i)[0]
+            else:
+                temp_string = 'Data dimensions = ' + str(hdul[group].data.field(i).shape)
+            print (' %2i' % i, ' %15s' % hdul[group].columns.names[i], ' %10s ' % hdul[group].columns.units[i], temp_string)
+            
+            #hdul[group].data.field(i)
+            #print (' * %2i' % i, ' %50s: ' % hdul[group].header.comments[i], hdul[group].header[i])
+            # print(repr(hdul[group].header)) - representation as it is in file
+            # print(list(hdul[group].header.keys())) - prints all keywords in header
+    
+    print ('\n ********************************************************************************** \n')
+    print ('\n Plotting figures... \n\n\n')
+    
+    
+    df_system_name = hdul[0].header[17]
+    df_obs_place = hdul[0].header[16]
+    TimeRes = (1 / float(hdul[0].header[14]))
+    df = 1000 * float(hdul[0].header[13][0:8])
+    
 
-    # Header 0
-    hdr = hdul[0].header
+    Label01 = hdul[1].data.field(14)[0, 0]
+    Label02 = hdul[1].data.field(14)[0, 1]
+    
+    df_description = hdul[2].data.field(0)[0]
+    no_analog_beams = int(hdul[2].data.field(6)[0])
+    no_digit_beams = int(hdul[2].data.field(7)[0])
+    
+    num_beamlet = np.zeros((no_digit_beams, ), dtype = int)     # number of beamlets (subbands) per beam
+    beam_list = np.zeros((no_digit_beams, 768), dtype = int)    # list of subbands
+    beam_freq = np.zeros((no_digit_beams, 768), dtype = float)  # list of frequencies
+    
+    
+    for beam in range(0, no_digit_beams):
+        num_beamlet[beam] = hdul[4].data.field('nbbeamlet')[beam]
+        beam_list[beam, :] = hdul[4].data.field('beamletlist')[beam]
+        beam_freq[beam, :] = hdul[4].data.field('freqlist')[beam]
+    
+    
+        frequency = np.zeros([no_digit_beams, num_beamlet[beam]])
+        frequency[:,:] = beam_freq[beam, 0:num_beamlet[beam]]
 
-    print(' \n')
-    print(' Length of header = ', len(hdr), '\n')
-    for i in range (len(hdr)):
-        print (' %2i' % i, ' %50s: ' % hdr.comments[i], hdr[i])
+        _, FreqPointsNum = frequency.shape
 
-    df_system_name = hdr[17]
-    df_obs_place = hdr[16]
-    TimeRes = (1 / float(hdr[14]))
-    df = 1000 * float(hdr[13][0:8])
-
-    print(' \n')
-
-    # print(repr(hdr)) - representation as it is in file
-    # print(list(hdr.keys())) - prints all keywords in header
-
-    '''
-    hdr1 = hdul[1].header
-    print(' \n')
-    print(' Length of header = ', len(hdr1), '\n')
-    for i in range (len(hdr1)):
-        print (' %2i' % i, ' %50s: ' % hdr1.comments[i], hdr1[i])
-    '''
-
-    # Header 1
-    cols = hdul[1].columns
-    data = hdul[1].data
-    #cols.info()
-    #print(data.shape)
-    #print(data.dtype.name)
-    for i in range (17):
-        print('   * ', cols.names[i], '   ', cols.units[i])
-        print(data.field(i), ' \n')
-
-    frequency_list = data.field(1)
-    Label01 = data.field(14)[0, 0]
-    Label02 = data.field(14)[0, 1]
-    print(' \n')
-
-    # Header 2
-    cols = hdul[2].columns
-    data = hdul[2].data
-    for i in range (16):
-        print('   * %15s' % cols.names[i], ' %5s ' % cols.units[i], ' %40s ' % data.field(i))
-
-
-    df_description = data.field(0)[0]
-
-
-    print(' \n')
-
-    cols = hdul[3].columns
-    data = hdul[3].data
-    for i in range (4):
-        print('   * %15s' % cols.names[i], ' %5s ' % cols.units[i], ' %40s ' % data.field(i))
-
-
-
-    print(' \n')
-
-    cols = hdul[4].columns
-    data = hdul[4].data
-    for i in range (4):
-        print('   * %15s' % cols.names[i], ' %5s ' % cols.units[i], ' %40s ' % data.field(i))
-
-
-    print(' \n')
-
-    cols = hdul[5].columns
-    data = hdul[5].data
-    for i in range (4):
-        print('   * ', cols.names[i], '   ', cols.units[i])
-        #print(data.field(i))
-
-    print(' \n')
-
-    cols = hdul[6].columns
-    data = hdul[6].data
-    for i in range (4):
-        print('   * ', cols.names[i], '   ', cols.units[i])
-        #print(data.field(i))
-
-    print(' \n')
-
-    cols = hdul[7].columns
-    data = hdul[7].data
-    for i in range (2):
-        print('   * ', cols.names[i], '   ', cols.units[i])
-        #print(data.field(i))
-    print(' \n')
-
-
-
-    beginIndex  = int(input('\n    First index of frequency subband:           '))
-    endIndex    = int(input('\n    Last index of frequency subband:            '))
-    FreqPointsNum = endIndex - beginIndex + 1
-
-    print('\n')
-
-    frequency = np.zeros(FreqPointsNum)
-    frequency[:] = frequency_list[0, beginIndex : endIndex+1]
-
-
-    dynamic_spectra = data.field(1)
+ 
+    dynamic_spectra = hdul[7].data.field(1)
     dynamic_spectra = dynamic_spectra[:, :, 0 : FreqPointsNum]
 
-    time_JD = data.field(0)
+    time_JD = hdul[7].data.field(0)
     time_line = Time(time_JD, format='jd', scale='utc')
     nt = time_line.shape[0]
     time_line_str = time_line.iso
@@ -175,19 +145,16 @@ def FileHeaderReaderFITS(foldpath, filename):
     dynamic_spectra1 = dynamic_spectra[:,0,:]
     dynamic_spectra2 = dynamic_spectra[:,1,:]
     Nim, pol, freq_num = dynamic_spectra.shape
-    print('   * Shape of dynamic spectra matrix: ', dynamic_spectra.shape)
-    print('   * Shape of frequency list:         ', frequency_list.shape)
-    print('   * Shape of time line:              ', nt)
-
-
+    
+    
     # ***    F I G U R E S   ***
 
     Vmin = np.min([np.min(dynamic_spectra1[0, 0:FreqPointsNum]), np.min(dynamic_spectra2[0, 0:FreqPointsNum])])
     Vmax = np.max([np.max(dynamic_spectra1[0, 0:FreqPointsNum]), np.max(dynamic_spectra2[0, 0:FreqPointsNum])])
 
-    TwoImmedSpectraPlot(frequency, dynamic_spectra1[0, 0:FreqPointsNum], dynamic_spectra2[0, 0:FreqPointsNum],
+    TwoImmedSpectraPlot(frequency[0,:], dynamic_spectra1[0, 0:FreqPointsNum], dynamic_spectra2[0, 0:FreqPointsNum],
                         Label01, Label02,
-                        frequency[0], frequency[len(frequency)-1], Vmin-3, Vmax+3,
+                        frequency[0,0], frequency[0, FreqPointsNum-1], Vmin-3, Vmax+3,
                         'Frequency, MHz', 'Intensity, dB',
                         'Immediate spectrum', 'for file ' + filename + ', Description: ' + df_description,
                         'FITS_Results/' + filename + '_Immediate spectrum.png',
@@ -211,12 +178,11 @@ def FileHeaderReaderFITS(foldpath, filename):
         figID, figMAX, TimeRes, df, sumDifMode, df_system_name, df_obs_place,
         filename, df_description, 'Intensity, dB', 'Intensity, dB', Nim,
         SpInFrame, FrameInChunk, ReceiverMode, TimeFigureScale, TimeScale,
-        Nim, frequency, FreqPointsNum, 'jet',
+        Nim, frequency[0,:], FreqPointsNum, 'jet',
         'Channel A', 'Channel B',
         'FITS_Results/',
         ' Initial dynamic spectrum fig.',
         currentDate, currentTime, Software_version, 300)
-
 
 
     Normalization_dB(dynamic_spectra1, FreqPointsNum, nt)
@@ -225,8 +191,7 @@ def FileHeaderReaderFITS(foldpath, filename):
 
     # *** FIGURE Dynamic spectrum channels A and B cleaned and normalized (python 3 new version) ***
     VminNorm = 0
-    VmaxNorm = 4
-
+    VmaxNorm = 15
 
 
     TwoDynSpectraPlot(np.flipud(dynamic_spectra1), np.flipud(dynamic_spectra2),
@@ -235,7 +200,7 @@ def FileHeaderReaderFITS(foldpath, filename):
         figID, figMAX, TimeRes, df, sumDifMode, df_system_name, df_obs_place,
         filename, df_description, 'Intensity, dB', 'Intensity, dB', Nim,
         SpInFrame, FrameInChunk, ReceiverMode, TimeFigureScale, TimeScale,
-        Nim, frequency, FreqPointsNum, 'jet',
+        Nim, frequency[0,:], FreqPointsNum, 'jet',
         'Channel A', 'Channel B',
         'FITS_Results/',
         ' Normalized dynamic spectrum fig.',
@@ -249,7 +214,6 @@ def FileHeaderReaderFITS(foldpath, filename):
 
 
 
-
     #return df_filename, df_filesize, df_system_name, df_obs_place, df_description, F_ADC, df_creation_timeUTC, ReceiverMode, ADRmode, sumDifMode, NAvr, TimeRes, frequency[0], frequency[FreqPointsNum-1], df, frequency, FFT_Size, SLine, Width, BlockSize
 
 
@@ -257,7 +221,7 @@ def FileHeaderReaderFITS(foldpath, filename):
 
 if __name__ == '__main__':
 
-    filename = '20190408_132500_BST.fits'
+    filename = '20190406_002900_BST.fits'
     foldpath = 'DATA/'
 
     print('\n\n * Parameters of the file: ')
