2b77ae4c57fce86797e0777ca64890a845649570
Serge Yerin <serge-yerin@users.noreply.github.com>
2019-07-29 19:03:28 +0300

    modified DAT reader to make pictures of minimal values instead of average

--- script_DAT_reader.py
+++ script_DAT_reader.py
@@ -6,45 +6,46 @@ Software_version = '2019.05.08'
 #                             P A R A M E T E R S                              *
 #*******************************************************************************
 # Path to data files
-common_path = 'DATA/'  #'e:/PYTHON/ra_data_processing-all/'
+common_path = 'e:/PYTHON/ra_data_processing-all/' # 'DATA/'
 
 # Directory of DAT file to be analyzed:
-filename = common_path + 'A170712_160219.adr_Data_chA.dat'
+filename = common_path + 'A130719_080208.jds_Data_chA.dat'
 
 # Types of data to get
 #typesOfData = ['chA', 'chB', 'C_m', 'C_p', 'CRe', 'CIm', 'A+B', 'A-B'] # !-!
-typesOfData = ['chA']
+typesOfData = ['chA', 'chB']
 
 # List of frequencies to build intensity changes vs. time and save to TXT file:
 #freqList = [10.0,15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0,75.0]
 #freqList = [9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
-freqList = [18.0,18.005,18.01,18.015,18.020,18.025]
+freqList = [4.0,5.0,6.0,7.0,8.0,8.05,8.1,8.15,8.5,9.0]
 
+averOrMin = 1                    # Use average value (0) per data block or minimum value (1)
 StartStopSwitch = 0              # Read the whole file (0) or specified time limits (1)
-SpecFreqRange = 0                # Specify particular frequency range (1) or whole range (0)
+SpecFreqRange = 1                # Specify particular frequency range (1) or whole range (0)
 VminMan = -120                   # Manual lower limit of immediate spectrum figure color range
 VmaxMan = -10                    # Manual upper limit of immediate spectrum figure color range
 VminNormMan = 0                  # Manual lower limit of normalized dynamic spectrum figure color range (usually = 0)
-VmaxNormMan = 10                 # Manual upper limit of normalized dynamic spectrum figure color range (usually = 15)
+VmaxNormMan = 18                 # Manual upper limit of normalized dynamic spectrum figure color range (usually = 15)
 RFImeanConst = 6                 # Constant of RFI mitigation (usually = 8)
 customDPI = 300                  # Resolution of images of dynamic spectra
 colormap = 'jet'                 # Colormap of images of dynamic spectra ('jet' or 'Greys')
-ChannelSaveTXT = 1               # Save intensities at specified frequencies to TXT file
+ChannelSaveTXT = 0               # Save intensities at specified frequencies to TXT file
 ChannelSavePNG = 1               # Save intensities at specified frequencies to PNG file
 ListOrAllFreq = 0                # Take all frequencies of a list to save TXT and PNG? 1-All, 0-List
 AmplitudeReIm = 2000 * 10**(-12) # Colour range of Re and Im dynamic spectra
                                  # 10 * 10**(-12) is typical value enough for CasA for interferometer of 2 GURT subarrays
 
 # Begin and end frequency of dynamic spectrum (MHz)
-freqStart = 8.0
-freqStop = 33.0
+freqStart = 0.0
+freqStop = 10.0
 
 # Begin and end time of dynamic spectrum ('yyyy-mm-dd hh:mm:ss')
 dateTimeStart = '2019-03-20 11:00:00'
 dateTimeStop =  '2019-03-20 12:30:00'
 
 # Begin and end frequency of TXT files to save (MHz)
-freqStartTXT = 8.0
+freqStartTXT = 0.0
 freqStopTXT = 33.0
 
 ################################################################################
@@ -321,8 +322,14 @@ for j in range(len(typesOfData)):  # Main loop by types of data to analyze
 
         if (typesOfData[j] == 'chA' or typesOfData[j]== 'chB'  or typesOfData[j] == 'A+B'):
         # If analyzing intensity - average and log data
-            with np.errstate(invalid='ignore'):
-                dataApp[:,0] = 10*np.log10(data.mean(axis=1)[:])
+            if averOrMin == 0:
+                with np.errstate(invalid='ignore'):
+                    dataApp[:,0] = 10*np.log10(data.mean(axis=1)[:])
+            elif averOrMin == 1:
+                with np.errstate(invalid='ignore'):
+                    dataApp[:,0] = 10*np.log10(np.amin(data, axis=1)[:])
+            else:
+                print('\n\n Error!!! Wrong value of parameters!')
             array = np.append(array, dataApp, axis=1)
             array[np.isnan(array)] = -120
 
@@ -440,7 +447,7 @@ for j in range(len(typesOfData)):  # Main loop by types of data to analyze
 
 
     # *** Cutting the array inside frequency range specified by user ***
-    if SpecFreqRange == 1 and (frequency[0]<freqStart<frequency[FreqPointsNum-1]) and (frequency[0]<freqStop<frequency[FreqPointsNum-1]) and (freqStart<freqStop):
+    if SpecFreqRange == 1 and (frequency[0]<=freqStart<=frequency[FreqPointsNum-1]) and (frequency[0]<=freqStop<=frequency[FreqPointsNum-1]) and (freqStart<freqStop):
         print ('\n You have chosen the frequency range', freqStart, '-', freqStop, 'MHz')
         A = []
         B = []
