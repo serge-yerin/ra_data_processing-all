6610c4d91370c381d15032ce1b31673b5a719125
serge-yerin <yerin.serge@gmail.com>
2019-08-17 11:46:35 +0300

    another try to improve

--- script_DAT_multifile_reader.py
+++ script_DAT_multifile_reader.py
@@ -7,7 +7,7 @@ import os
 #                             P A R A M E T E R S                              *
 #*******************************************************************************
 # Path to data files
-path_to_data = '/media/data/PYTHON/ra_data_processing-all/' # 'DATA/'
+path_to_data = 'DATA/'  # '/media/data/PYTHON/ra_data_processing-all/'
 
 # Path to intermediate data files and results
 path_to_results = os.path.dirname(os.path.realpath(__file__)) + '/'  # 'd:/PYTHON/ra_data_processing-all/' # 'DATA/'
@@ -56,6 +56,7 @@ freqStopTXT = 33.0
 import numpy as np
 import time
 from datetime import datetime, timedelta
+from astropy.time import Time, TimeDelta
 
 # My functions
 from package_ra_data_files_formats.DAT_file_reader import DAT_file_reader
@@ -96,6 +97,10 @@ data_files_name_list = []
 for i in range (len(timeline_file_name_list)):
     data_files_name_list.append(timeline_file_name_list[i][-31:-13])
 
+
+# Checking and printing
+
+
 # Loop by data types selected by user
 for type_of_data in typesOfData:
     dat_files_list = []
@@ -108,7 +113,7 @@ for type_of_data in typesOfData:
     for file_no in range (len(dat_files_list)):
 
         #  *** Find if the file is CasA of SygA ***
-        file = open(dat_files_list[file_no], 'rb')
+        file = open(path_to_data + dat_files_list[file_no], 'rb')
 
         # Check is it the file of ADR or JDS data
         df_filename = file.read(32).decode('utf-8').rstrip('\x00')      # Initial data file name
@@ -125,7 +130,7 @@ for type_of_data in typesOfData:
 
             [df_filename, df_filesize, df_system_name, df_obs_place, df_description,
                     CLCfrq, df_creation_timeUTC, SpInFile, ReceiverMode, Mode, Navr, TimeRes, fmin, fmax,
-                    df, frequency, FreqPointsNum, dataBlockSize] = FileHeaderReaderJDS(dat_files_list[file_no], 0, 0)
+                    df, frequency, FreqPointsNum, dataBlockSize] = FileHeaderReaderJDS(path_to_data + dat_files_list[file_no], 0, 0)
 
         if ('3c461' in df_description.lower()) or ('cas' in df_description.lower()):
             source = '3C461'
@@ -142,13 +147,60 @@ for type_of_data in typesOfData:
 
         if df_filename[-4:] == '.jds':
             date = '20'+df_filename[5:7]+'-'+df_filename[3:5]+'-'+df_filename[1:3]
-
         if df_filename[-4:] == '.adr':
             date = '20'+df_filename[1:3]+'-'+df_filename[3:5]+'-'+df_filename[5:7]
 
-        culm_time = culmination_time_utc(source, date, 0)
+        #culm_time = culmination_time_utc(source, date, 0)
+        culm_time = date + ' 23:00:00'
 
-        print (date, culm_time)
+        culm_time = Time(culm_time)
+        start_time = culm_time - TimeDelta(3600, format = 'sec')
+        end_time  = culm_time + TimeDelta(3600, format = 'sec')
+
+        start_time_str = str(start_time)[0:19]
+        end_time_str = str(end_time)[0:19]
+
+        print(start_time_str, end_time_str)
+
+
+        # *** Reading timeline file ***
+        name = data_files_name_list[file_no] + '_Timeline.txt'
+        TLfile = open(name, 'r')
+        timeline = []
+        for line in TLfile:
+            timeline.append(str(line))
+        TLfile.close()
+
+        '''
+        if StartStopSwitch == 1:  # If we read only specified time limits of files
+
+            # *** Converting text to ".datetime" format ***
+            dt_timeline = []
+            for i in range (len(timeline)):  # converting text to ".datetime" format
+
+                # Check is the uS field is empty. If so it means it is equal to '000000'
+                uSecond = timeline[i][20:26]
+                if len(uSecond) < 2: uSecond = '000000'
+
+                dt_timeline.append(datetime(int(timeline[i][0:4]), int(timeline[i][5:7]), int(timeline[i][8:10]), int(timeline[i][11:13]), int(timeline[i][14:16]), int(timeline[i][17:19]), int(uSecond)))
+
+            dt_dateTimeStart = datetime(int(dateTimeStart[0:4]), int(dateTimeStart[5:7]), int(dateTimeStart[8:10]), int(dateTimeStart[11:13]), int(dateTimeStart[14:16]), int(dateTimeStart[17:19]), 0)
+            dt_dateTimeStop = datetime(int(dateTimeStop[0:4]), int(dateTimeStop[5:7]), int(dateTimeStop[8:10]), int(dateTimeStop[11:13]), int(dateTimeStop[14:16]), int(dateTimeStop[17:19]), 0)
+
+            # *** Showing the time limits of file and time limits of chosen part
+            print ('\n\n                               Start                         End \n')
+            print ('  File time limits:   ', dt_timeline[0],' ', dt_timeline[len(timeline)-1], '\n')
+            print ('  Chosen time limits: ', dt_dateTimeStart, '        ', dt_dateTimeStop, '\n')
+
+            # Verifying that chosen time limits are inside file and are correct
+            if (dt_timeline[len(timeline)-1]>=dt_dateTimeStart>dt_timeline[0])and(dt_timeline[len(timeline)-1]>dt_dateTimeStop>=dt_timeline[0])and(dt_dateTimeStop>dt_dateTimeStart):
+                print ('  Time is chosen correctly! \n\n')
+            else:
+                print ('  ERROR! Time is chosen out of file limits!!! \n\n')
+                sys.exit('         Program stopped')
+
+            '''
+
 
 
         '''
