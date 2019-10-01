bbec619c8aa9f44fc902ba70edabecf16042544e
Serge Yerin <serge-yerin@users.noreply.github.com>
2019-10-01 23:04:46 +0300

    sources and pulsar catalogues corrected

--- package_astronomy/catalogue_sources.py
+++ package_astronomy/catalogue_sources.py
@@ -4,10 +4,11 @@
 
 def catalogue_sources(source_name):
     source_name = source_name.upper()
-    if   ('CAS A' or '3C461' or '4C 58.40') in source_name: source_ra = '23h23m24.0s';   source_dec = '+58d48m54.0s'
-    elif ('SYG A' or '3C405' or '4C 40.40') in source_name: source_ra = '19h59m28.356s'; source_dec = '+40d44m02.0967s'
-    elif ('VIR A' or '3C274' or '4C 12.45') in source_name: source_ra = '12h30m49.423s'; source_dec = '+12d23m28.0439s'
-    elif ('CRAB'  or '3C144' or '4C 21.19') in source_name: source_ra = '05h34m31.94s';  source_dec = '+22d00m52.2s'
+    if   'CAS A' or '3C461' or '4C 58.40' in source_name: source_ra = '23h23m24.0s';   source_dec = '+58d48m54.0s'
+    elif 'SYG A' or '3C405' or '4C 40.40' in source_name: source_ra = '19h59m28.356s'; source_dec = '+40d44m02.0967s'
+    elif 'VIR A' or '3C274' or '4C 12.45' in source_name: source_ra = '12h30m49.423s'; source_dec = '+12d23m28.0439s'
+    elif 'CRAB'  or '3C144' or '4C 21.19' in source_name: source_ra = '05h34m31.94s';  source_dec = '+22d00m52.2s'
+    else: print('   !!! Source not found !!!')
     return source_ra, source_dec
 
 ################################################################################
