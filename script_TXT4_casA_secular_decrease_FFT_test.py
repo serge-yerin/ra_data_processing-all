0473f7bb36a1fd6df147a0b645bc39141731b071
Serge Yerin <serge-yerin@users.noreply.github.com>
2019-10-05 22:42:36 +0300

    improves FFT test

--- script_TXT4_casA_secular_decrease_FFT_test.py
+++ script_TXT4_casA_secular_decrease_FFT_test.py
@@ -162,7 +162,7 @@ responce_plot(1 * filtered_signal_re, 'Interferometric responce  after FFT and i
 #*******************************************************************************
 # Specter of signal without higher frequencies
 experiment_spectra[0] = 0.000000001 #np.nan
-experiment_spectra[50:] = 0.000000001
+experiment_spectra[25:] = 0.000000001
 ampl_spectra = np.abs(experiment_spectra)
 max_index = np.argmax(ampl_spectra)
 
@@ -252,6 +252,7 @@ spectra_plot(ampl_spectra, max_index, ymax,
 
 filtered_signal_im = np.imag(np.fft.ifft(experiment_spectra))
 filtered_signal_re = np.real(np.fft.ifft(experiment_spectra))
+signal_5_harmonics = 2 * filtered_signal_re
 
 responce_plot(2 * filtered_signal_re, 'Interferometric responce only 5 harmonics',
             '02 - Interferometric responce 05.png', path_to_data, Vmin, Vmax, date_time)
@@ -327,6 +328,111 @@ filtered_signal_re = np.real(np.fft.ifft(experiment_spectra))
 
 responce_plot(2 * filtered_signal_re, 'Interferometric responce only 1 harmonic',
             '02 - Interferometric responce 07.png', path_to_data, Vmin, Vmax, date_time)
+
+
+
+#*******************************************************************************
+# Signal without lower frequencies
+
+spectra = np.fft.fft(y_value[0, :])
+spectra[0 : 25] = 0.000000001
+spectra[-25 : ] = 0.000000001
+ampl_spectra = np.abs(spectra)
+
+
+rc('font', size = 6, weight='bold')
+fig = plt.figure(figsize = (12, 5))
+fig.suptitle('Spectra of the higher frequencies only', fontsize = 8, fontweight='bold')
+ax1 = fig.add_subplot(121)
+ax1.set_title('Spectra', fontsize = 6)
+ax1.plot(ampl_spectra, linestyle = '-', linewidth = '2.0', alpha = 1.0, label = 'Measurements')
+ax1.scatter(np.linspace(0, len(ampl_spectra)-1, num = len(ampl_spectra)), ampl_spectra, color = 'r')
+#ax1.annotate(str(np.round(ymax,5)),  xy=(max_index+15, ymax), fontsize = 6, ha='center') # xy=(xmax, 2 + 0.3)
+ax1.set_xlim([0, int(len(ampl_spectra)/2)])
+ax2 = fig.add_subplot(122)
+ax2.set_title('Spectra', fontsize = 6)
+ax2.plot(10 * np.log10(ampl_spectra), linestyle = '-', linewidth = '2.0', alpha = 1.0, label = 'Measurements')
+ax2.scatter(np.linspace(0, len(ampl_spectra)-1, num = len(ampl_spectra)), 10 * np.log10(ampl_spectra), color = 'r')
+#ax2.annotate(str(np.round(10 * np.log10(ymax),5)),  xy=(max_index+15, 10 * np.log10(ymax)), fontsize = 6, ha='center')
+ax2.set_xlim([0, int(len(ampl_spectra)/2)])
+#ax2.set_ylim([-100, -50])
+pylab.savefig(path_to_data + '01 - Spectra of interferometric responce 08.png', bbox_inches = 'tight', dpi = 160)
+plt.close('all')
+
+
+signal_re = np.real(np.fft.ifft(spectra))
+
+
+
+rc('font', size = 6, weight='bold')
+fig = plt.figure(figsize = (9, 5))
+fig.suptitle('Interferometric responce of high frequencies', fontsize = 8, fontweight='bold')
+ax1 = fig.add_subplot(111)
+plt.axvline(x = x_center, linewidth = '0.5' , color = 'r') #, alpha=0.5
+ax1.plot(2 * signal_re, linestyle = '-', linewidth = '1.00', label = 'Filtered')
+#ax1.plot(y_value[0, :], linestyle = '-', linewidth = '1.00', label = 'Measured')
+ax1.legend(loc = 'upper right', fontsize = 6)
+ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+ax1.set_ylim([Vmin, Vmax])
+ax1.set_xlim([0, b-1])
+ax1.set_ylabel('Intensity, a.u.', fontsize=6, fontweight='bold')
+ax1.set_xlabel('UTC Date and time, YYYY-MM-DD HH:MM:SS.ms', fontsize=6, fontweight='bold')
+ax1.xaxis.set_major_locator(mtick.LinearLocator(7))
+text = ax1.get_xticks().tolist()
+for i in range(len(text)):
+    k = int(text[i])
+    text[i] = str(date_time[k][0:11] + '\n' + date_time[k][11:23])
+ax1.set_xticklabels(text, fontsize = 6, fontweight = 'bold')
+fig.subplots_adjust(top=0.92)
+pylab.savefig(path_to_data + '02 - Interferometric responce 08.png', bbox_inches = 'tight', dpi = 160)
+plt.close('all')
+
+rc('font', size = 6, weight='bold')
+fig = plt.figure(figsize = (9, 5))
+fig.suptitle('Interferometric responce of high frequencies vs. initial signal', fontsize = 8, fontweight='bold')
+ax1 = fig.add_subplot(111)
+plt.axvline(x = x_center, linewidth = '0.5' , color = 'r') #, alpha=0.5
+ax1.plot(2 * signal_re, linestyle = '-', linewidth = '1.00', label = 'Filtered')
+ax1.plot(y_value[0, :], linestyle = '-', linewidth = '1.00', label = 'Measured')
+ax1.legend(loc = 'upper right', fontsize = 6)
+ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+ax1.set_ylim([Vmin, Vmax])
+ax1.set_xlim([0, b-1])
+ax1.set_ylabel('Intensity, a.u.', fontsize=6, fontweight='bold')
+ax1.set_xlabel('UTC Date and time, YYYY-MM-DD HH:MM:SS.ms', fontsize=6, fontweight='bold')
+ax1.xaxis.set_major_locator(mtick.LinearLocator(7))
+text = ax1.get_xticks().tolist()
+for i in range(len(text)):
+    k = int(text[i])
+    text[i] = str(date_time[k][0:11] + '\n' + date_time[k][11:23])
+ax1.set_xticklabels(text, fontsize = 6, fontweight = 'bold')
+fig.subplots_adjust(top=0.92)
+pylab.savefig(path_to_data + '02 - Interferometric responce 09.png', bbox_inches = 'tight', dpi = 160)
+plt.close('all')
+
+
+rc('font', size = 6, weight='bold')
+fig = plt.figure(figsize = (9, 5))
+fig.suptitle('Interferometric responce of high frequenciesvs. 5 harmonics', fontsize = 8, fontweight='bold')
+ax1 = fig.add_subplot(111)
+plt.axvline(x = x_center, linewidth = '0.5' , color = 'r') #, alpha=0.5
+ax1.plot(2 * signal_re, linestyle = '-', linewidth = '1.00', label = 'Filtered high')
+ax1.plot(signal_5_harmonics, linestyle = '-', linewidth = '1.00', label = 'Filtered 5 harmonics')
+ax1.legend(loc = 'upper right', fontsize = 6)
+ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+ax1.set_ylim([Vmin, Vmax])
+ax1.set_xlim([0, b-1])
+ax1.set_ylabel('Intensity, a.u.', fontsize=6, fontweight='bold')
+ax1.set_xlabel('UTC Date and time, YYYY-MM-DD HH:MM:SS.ms', fontsize=6, fontweight='bold')
+ax1.xaxis.set_major_locator(mtick.LinearLocator(7))
+text = ax1.get_xticks().tolist()
+for i in range(len(text)):
+    k = int(text[i])
+    text[i] = str(date_time[k][0:11] + '\n' + date_time[k][11:23])
+ax1.set_xticklabels(text, fontsize = 6, fontweight = 'bold')
+fig.subplots_adjust(top=0.92)
+pylab.savefig(path_to_data + '02 - Interferometric responce 10.png', bbox_inches = 'tight', dpi = 160)
+plt.close('all')
 
 
 
