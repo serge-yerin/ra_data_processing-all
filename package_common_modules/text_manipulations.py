122bdc6d3248e748aa2132bd62e4823c95538981
Serge Yerin <serge-yerin@users.noreply.github.com>
2019-10-14 22:31:40 +0300

    added reading of 3-column txt file and script to build the antenna SND for DSP in two subbands

--- package_common_modules/text_manipulations.py
+++ package_common_modules/text_manipulations.py
@@ -34,3 +34,31 @@ def read_date_time_and_one_value_txt (filenamelist):
         x_value.append(x_val)
         y_value.append(y_val)
     return x_value, y_value
+
+
+
+
+def read_frequency_and_two_values_txt(filenamelist):
+    '''
+    Reading txt files with frequecy and two value column data
+    '''
+    x_value = []
+    y1_value = []
+    y2_value = []
+
+    for file in range (len(filenamelist)):
+        file = open(filenamelist[file], 'r')
+        x_val = []
+        y1_val = []
+        y2_val = []
+        for line in file:
+            words = line.rstrip().split()
+            x_val.append(float(words[0]))
+            y1_val.append(float(words[1]))
+            y2_val.append(float(words[2]))
+        file.close()
+
+        x_value.append(x_val)
+        y1_value.append(y1_val)
+        y2_value.append(y2_val)
+    return x_value, y1_value, y2_value
--- script_TXT_JDS_SND.py
+++ script_TXT_JDS_SND.py
@@ -14,15 +14,10 @@ Vmin = -500 * 10**(-12)
 Vmax =  500 * 10**(-12)
 
 # TXT files to be analyzed:
-filename.append(common_path + 'C091019_125557_chA Intensity variation at 31.002 MHz.txt')
-#filename.append(common_path + 'A170712_160219_chA Intensity variation at 10.01 MHz.txt')
-#filename.append(common_path + 'A170712_160219_chA Intensity variation at 11.006 MHz.txt')
-#filename.append(common_path + 'A170712_160219_chA Intensity variation at 12.012 MHz.txt')
-#filename.append(common_path + 'A170712_160219_chA Intensity variation at 13.008 MHz.txt')
-#filename.append(common_path + 'A170712_160219_chA Intensity variation at 14.014 MHz.txt')
-#filename.append(common_path + 'A170712_160219_chA Intensity variation at 15.01 MHz.txt')
-#filename.append(common_path + 'C020419_082413_CRe Intensity variation at 18.031 MHz.txt')
-
+sky_file = common_path + 'Specter_C091019_125557.txt'
+off_file = common_path + 'Specter_C111019_080855.txt'
+open_file = common_path + 'Specter_C111019_065836.txt'
+short_file = common_path + 'Specter_C111019_081803.txt'
 
 customDPI = 300                     # Resolution of images of dynamic spectra
 
@@ -37,14 +32,14 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import time
 
-from package_common_modules.text_manipulations import find_between, read_date_time_and_one_value_txt
-from package_plot_formats.plot_formats import OneValueWithTimePlot
+from package_common_modules.text_manipulations import read_frequency_and_two_values_txt
+
 ################################################################################
 #*******************************************************************************
 #                          M A I N    P R O G R A M                            *
 #*******************************************************************************
 print ('\n\n\n\n\n\n\n\n   ****************************************************')
-print ('   *          TXT data files reader  v1.0             *      (c) YeS 2016')
+print ('   *          TXT data files reader  v1.0             *      (c) YeS 2019')
 print ('   ****************************************************      \n\n\n')
 
 startTime = time.time()
@@ -52,17 +47,110 @@ currentTime = time.strftime("%H:%M:%S")
 currentDate = time.strftime("%d.%m.%Y")
 print ('  Today is ', currentDate, ' time is ', currentTime, '\n')
 
+
 # *** Creating a folder where all pictures will be stored (if it doesn't exist) ***
 newpath = "TXT_Results"
 if not os.path.exists(newpath):
     os.makedirs(newpath)
+
+
 
 #*******************************************************************************
 #                          R E A D I N G   D A T A                             *
 #*******************************************************************************
 
 # *** Reading files ***
-[x_value, y_value] = read_date_time_and_one_value_txt (filename)
+[x_value, y1_value, y2_value] = read_frequency_and_two_values_txt ([sky_file])
+
+
+freq_num = len(x_value[0])
+frequencies = np.zeros(2 * freq_num)
+sky_responce = np.zeros(2 * freq_num)
+
+for i in range(freq_num):
+    frequencies[i] = x_value[0][i]
+    frequencies[i + freq_num] = x_value[0][i] + 33.0
+
+for i in range(freq_num):
+    sky_responce[i] = y1_value[0][i]
+    sky_responce[i + freq_num] = y2_value[0][freq_num - i - 1]
+
+
+[x_value, y1_value, y2_value] = read_frequency_and_two_values_txt ([off_file])
+
+off_responce = np.zeros(2 * freq_num)
+for i in range(freq_num):
+    off_responce[i] = y1_value[0][i]
+    off_responce[i + freq_num] = y2_value[0][freq_num - i - 1]
+
+[x_value, y1_value, y2_value] = read_frequency_and_two_values_txt ([open_file])
+
+open_responce = np.zeros(2 * freq_num)
+for i in range(freq_num):
+    open_responce[i] = y1_value[0][i]
+    open_responce[i + freq_num] = y2_value[0][freq_num - i - 1]
+
+[x_value, y1_value, y2_value] = read_frequency_and_two_values_txt ([short_file])
+
+short_responce = np.zeros(2 * freq_num)
+for i in range(freq_num):
+    short_responce[i] = y1_value[0][i]
+    short_responce[i + freq_num] = y2_value[0][freq_num - i - 1]
+
+
+#*******************************************************************************
+#                                F I G U R E S                                 *
+#*******************************************************************************
+
+
+
+print ('\n\n\n  *** Building images *** \n\n')
+
+
+rc('font', size = 6, weight='bold')
+fig = plt.figure(figsize = (9, 5))
+ax1 = fig.add_subplot(111)
+ax1.plot(frequencies, sky_responce, label = 'Sky')
+ax1.plot(frequencies, off_responce, label = 'Power OFF')
+ax1.plot(frequencies, open_responce, label = 'Open circuit (XX)')
+ax1.plot(frequencies, short_responce, label = 'Short circuit (KZ)')
+ax1.legend(loc = 'upper right', fontsize = 6)
+ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+if y_auto == 0: ax1.set_ylim([Vmin, Vmax])
+ax1.set_ylabel('Intensity, dB', fontsize=6, fontweight='bold')
+ax1.set_title('   ', fontsize = 6)
+ax1.set_xlabel('Frequency, MHz', fontsize=6, fontweight='bold')
+fig.subplots_adjust(top=0.92)
+fig.suptitle('File: ', fontsize = 8, fontweight='bold')
+fig.text(0.79, 0.03, 'Processed '+currentDate+ ' at '+currentTime, fontsize=4, transform=plt.gcf().transFigure)
+fig.text(0.11, 0.03, 'Software version: '+Software_version+', yerin.serge@gmail.com, IRA NASU', fontsize=4, transform=plt.gcf().transFigure)
+pylab.savefig(newpath + '/' + ' 01 - All txt data used.png', bbox_inches = 'tight', dpi = 160)
+plt.close('all')
+
+
+rc('font', size = 6, weight='bold')
+fig = plt.figure(figsize = (9, 5))
+ax1 = fig.add_subplot(111)
+ax1.plot(frequencies, sky_responce - off_responce, label = 'Sky - Power OFF')
+ax1.plot(frequencies, sky_responce - open_responce, label = 'Sky - Open circuit (XX)')
+ax1.plot(frequencies, sky_responce - short_responce, label = 'Sky - Short circuit (KZ)')
+ax1.legend(loc = 'upper right', fontsize = 6)
+ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+ax1.set_ylim([-20, 80])
+ax1.set_ylabel('Intensity, dB', fontsize=6, fontweight='bold')
+ax1.set_title('   ', fontsize = 6)
+ax1.set_xlabel('Frequency, MHz', fontsize=6, fontweight='bold')
+fig.subplots_adjust(top=0.92)
+fig.suptitle('File: ', fontsize = 8, fontweight='bold')
+fig.text(0.79, 0.03, 'Processed '+currentDate+ ' at '+currentTime, fontsize=4, transform=plt.gcf().transFigure)
+fig.text(0.11, 0.03, 'Software version: '+Software_version+', yerin.serge@gmail.com, IRA NASU', fontsize=4, transform=plt.gcf().transFigure)
+pylab.savefig(newpath + '/' + ' 02 - Differences.png', bbox_inches = 'tight', dpi = 160)
+plt.close('all')
+
+
+
+
+'''
 
 y_value = np.array(y_value)
 a, b = y_value.shape
@@ -81,35 +169,6 @@ print (list_text_freqs)
 
 parent_filename = find_between(filename[0], common_path, ' Intensity')
 
-#*******************************************************************************
-#                                F I G U R E S                                 *
-#*******************************************************************************
-
-print ('\n\n\n  *** Building images *** \n\n')
-
-
-rc('font', size = 6, weight='bold')
-fig = plt.figure(figsize = (9, 5))
-ax1 = fig.add_subplot(111)
-for i in range (a):
-    ax1.plot(y_value[i, :], linestyle = '-', linewidth = '1.00', label = text_freqs[i])
-ax1.legend(loc = 'upper right', fontsize = 6)
-ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
-if y_auto == 0: ax1.set_ylim([Vmin, Vmax])
-ax1.set_ylabel('Intensity, dB', fontsize=6, fontweight='bold')
-ax1.set_title('   ', fontsize = 6)
-ax1.set_xlabel('UTC Date and time, YYYY-MM-DD HH:MM:SS.ms', fontsize=6, fontweight='bold')
-text = ax1.get_xticks().tolist()
-for i in range(len(text)-1):
-    k = int(text[i])
-    text[i] = str(date_time[i][0:11] + '\n' + date_time[i][11:23])
-ax1.set_xticklabels(text, fontsize = 6, fontweight = 'bold')
-fig.subplots_adjust(top=0.92)
-fig.suptitle('File: '+parent_filename, fontsize = 8, fontweight='bold')
-fig.text(0.79, 0.03, 'Processed '+currentDate+ ' at '+currentTime, fontsize=4, transform=plt.gcf().transFigure)
-fig.text(0.11, 0.03, 'Software version: '+Software_version+', yerin.serge@gmail.com, IRA NASU', fontsize=4, transform=plt.gcf().transFigure)
-pylab.savefig(newpath + '/' + parent_filename + ' 01 - All txt data used.png', bbox_inches = 'tight', dpi = 160)
-plt.close('all')
 
 
 # Averaging the curves
@@ -130,7 +189,7 @@ OneValueWithTimePlot(timeline, average, 'Averaged values', 0, 1, Vmin, Vmax, 1, 
                         'UTC Date and time, YYYY-MM-DD HH:MM:SS.ms', 'Intensity, dB',
                         'File: '+parent_filename+' at '+ list_text_freqs +' MHz', '  ', FileName,
                         currentDate, currentTime, Software_version)
-
+'''
 
 
 endTime = time.time()
