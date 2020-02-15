corrected averaging figure to averaged spectra from WF

package_sandbox/script_JDS_WF_reader.py
@@ -135,9 +135,6 @@ for fileNo in range(len(fileList)):   # loop by files
             # *** Data file header read ***
             file_header = file.read(1024)
 
-
-        #file_header[624] = Navr * no_of_spectra_to_average
-
         # *** Creating a name for long timeline TXT file ***
         TLfile_name = df_filename + '_Timeline.txt'
         TLfile = open(TLfile_name, 'w')  # Open and close to delete the file with the same name
@@ -147,7 +144,7 @@ for fileNo in range(len(fileList)):   # loop by files
         file_data_A_name = df_filename + '_Data_chA.dat'
         file_data_A = open(file_data_A_name, 'wb')
         file_data_A.write(file_header)
-        file_data_A.seek(636) # 708 724
+        file_data_A.seek(636)  # Navr place in header
         file_data_A.write(bytes([np.int32(Navr * no_of_spectra_to_average)]))
         file_data_A.close()
 
@@ -155,6 +152,8 @@ for fileNo in range(len(fileList)):   # loop by files
             file_data_B_name = df_filename + '_Data_chB.dat'
             file_data_B = open(file_data_B_name, 'wb')
             file_data_B.write(file_header)
+            file_data_A.seek(636)   # Navr place in header
+            file_data_A.write(bytes([np.int32(Navr * no_of_spectra_to_average)]))
             file_data_B.close()
         
         del file_header