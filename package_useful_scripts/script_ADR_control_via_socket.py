50403705069ede3caf65f052ef5590e471c7d48f
serge-yerin <yerin.serge@gmail.com>
2020-04-10 19:47:46 +0300

    first light

--- package_useful_scripts/script_ADR_control_via_socket.py
+++ package_useful_scripts/script_ADR_control_via_socket.py
@@ -3,17 +3,13 @@
 #*************************************************************
 #                        PARAMETERS                          *
 #*************************************************************
-
 #*************************************************************
-
 
 #*************************************************************
 #                   IMPORT LIBRARIES                         *
 #*************************************************************
 import socket
 from threading import *
-
-
 
 #*************************************************************
 #                       MAIN PROGRAM                         *
@@ -22,16 +18,31 @@ from threading import *
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '192.168.1.171'
 port = 38386
-login = 'control'
-print ' Host: ', host
-print ' Port: ', port
+login = 'view' #'control'
+print (' Host: ', host)
+print (' Port: ', port)
 serversocket.connect((host, port))
 
 def ts(str):
+    ####serversocket.send('get prc/dsp/ctl/mdo'.encode())
+    serversocket.send('ADRSCTRL'.encode())
+    ctrl = 0
+    register_cc_msg = bytearray([108,0,0,0])
+    register_cc_msg.extend(b'YeS\0                                                            ')  # Name 64 bytes
+    register_cc_msg.extend('adrs\0                           '.encode())                          # Password 32 bytes
+    register_cc_msg.extend([0, 0, 0, 0])                                                         # Priv 4 bytes
+    register_cc_msg.extend([ctrl, 0, 0, 0])                                                      # CTRL 4 bytes
+    register_cc_msg = bytes(register_cc_msg)
+    print(' Length: ', len(register_cc_msg))
+    #print(' Sent message: ', register_cc_msg)
+
+    serversocket.send(register_cc_msg)
+
+    data = serversocket.recv(200).decode()
+    print (' Received message: ', data)
+
     serversocket.send('get prc/dsp/ctl/mdo'.encode())
-    data = ''
     data = serversocket.recv(1024).decode()
-    print data
+    print (' Received message: ', data)
 
 ts(str)
- 
