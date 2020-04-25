0b0af9ec09cca2a4b0a37dd6c9a7216b0901c964
Serge Yerin <yerin.serge@gmail.com>
2020-04-26 00:19:12 +0300

    observation schedule read function added

---
+++ package_receiver_control/f_read_schedule_txt_for_adr.py
@@ -0,0 +1,92 @@
+# Python 3
+import sys
+from datetime import datetime
+from package_common_modules.text_manipulations import find_between
+
+#*************************************************************
+#                       MAIN FUNCTION                        *
+#*************************************************************
+def f_read_schedule_txt_for_adr(schedule_txt_file):
+    '''
+    Function reads a message from ADR radio astronomy receiver
+    Input parameters:
+        schedule_txt_file        - name of txt file with observation schedule
+    Output parameters:
+    '''
+    print('\n * Reading observation schedule...')
+    schedule = []
+    file = open(schedule_txt_file, "r")
+    for line in file:
+        if line.strip().startswith('#'):
+            pass
+        elif line.strip().startswith('START:'):
+            line = line.replace(' ', '').rstrip('\n')
+            start_time =        find_parameter_value(line, 'START:')
+            fft_size =          find_parameter_value(line, 'FFT:')
+            time_resolution =   find_parameter_value(line, 'DT:')
+            data_directory =    find_parameter_value(line, 'DIR:')
+            file_description =  find_parameter_value(line, 'DESCR:')
+        elif line.strip().startswith('STOP:'):
+            line = line.replace(' ', '').rstrip('\n')
+            stop_time = find_parameter_value(line, 'STOP:')
+            copy_or_not = find_parameter_value(line, 'COPY:')
+            process_or_not = find_parameter_value(line, 'PROC:')
+            # Adding parameters to list
+            schedule.append([start_time[:10]+' '+start_time[10:], stop_time[:10]+' '+stop_time[10:], fft_size,
+                             time_resolution, data_directory, file_description, copy_or_not, process_or_not])
+        else:
+            pass
+
+    # Check time correctness (later then now, start is before stop): storing them in datetime format in one list
+    time_line = []
+    for i in range (len(schedule)):
+        time_point = schedule[i][0]
+        dt_time = datetime(int(time_point[0:4]), int(time_point[5:7]), int(time_point[8:10]),
+                                int(time_point[11:13]), int(time_point[14:16]), int(time_point[17:19]), 0)
+        time_line.append(dt_time)
+        time_point = schedule[i][1]
+        dt_time = datetime(int(time_point[0:4]), int(time_point[5:7]), int(time_point[8:10]),
+                           int(time_point[11:13]), int(time_point[14:16]), int(time_point[17:19]), 0)
+        time_line.append(dt_time)
+
+    # Verifying that time limits in list go one after another
+    for i in range (2*len(schedule)-1):
+        if (time_line[i+1] > time_line[i]):
+            pass
+        else:
+            print('\n  ERROR! Time is not in right order!!! \n\n')
+            sys.exit('         Program stopped')
+
+    # Check if the first time in the list is in future
+    now = datetime.now()
+    diff = int((time_line[0] - now).total_seconds())
+    if diff <= 0:
+        print('\n  ERROR! Time is in the past!!! \n\n')
+        sys.exit('         Program stopped')
+
+    # check FFT value correctness
+
+
+    print('\n Number of observations found: ', len(schedule))
+
+
+    return schedule
+
+def find_parameter_value(line, string_name):
+    if string_name in line:
+        value = (find_between(line, string_name, ','))
+        if value == 'Error!':
+            begin = line.index(string_name) + len(string_name)
+            value = line[begin:]
+    else:
+        value = None
+    return value
+
+################################################################################
+
+if __name__ == '__main__':
+    schedule_txt_file = 'Example of observations script.txt'
+    schedule = f_read_schedule_txt_for_adr(schedule_txt_file)
+    for i in range (len(schedule)):
+        print(schedule[i])
+
