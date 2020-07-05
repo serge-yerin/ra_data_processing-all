e0bdaefb311fdb3ffa6fea3d70872a1899395ef0
serge-yerin <yerin.serge@gmail.com>
2020-07-05 16:41:21 +0300

    cleaned and made standard names, no changes

--- package_ra_data_processing/spectra_normalization.py
+++ package_ra_data_processing/spectra_normalization.py
@@ -4,46 +4,44 @@
 import numpy as np
 
 # ***  ***
-def Normalization_dB(Data, FreqPointsNum, Num_of_apectra):
+def Normalization_dB(data, freq_points_num, num_of_spectra):
     '''
     Normalizing amplitude-frequency responce (spectra) in dB!!!
     Input parameters:
-        Data - input array
-        FreqPointsNum - number of frequency channels (data array dimension)
+        data - input array
+        freq_points_num - number of frequency channels (data array dimension)
         Num_of_apectra - number of spectra (data array dimension)
     Output parameters:
-        Data - result normalized data array
+        data - result normalized data array
     '''
-    DataMinf = []
-    DataMinNorm = [0 for col in range(FreqPointsNum)]
-    for i in range (FreqPointsNum):
-        DataMinf = Data[:, i]                                       # taking 1 line of matrix to find 5 minimum values
-        DataMinNorm[i] = np.mean(DataMinf[DataMinf.argsort()[:5]])  # calculating mean of 5 minimum values knowing their indexes
+    data_min_norm = [0 for col in range(freq_points_num)]
+    for i in range (freq_points_num):
+        data_min_find = data[:, i]                                       # taking 1 line of matrix to find 5 minimum values
+        data_min_norm[i] = np.mean(data_min_find[data_min_find.argsort()[:5]])  # calculating mean of 5 minimum values knowing their indexes
 
-    for k in range (Num_of_apectra):
+    for k in range (num_of_spectra):
         with np.errstate(invalid='ignore'):
-            Data[k, :] = Data[k, :] - DataMinNorm[:]
-    return Data
+            data[k, :] = data[k, :] - data_min_norm[:]
+    return data
 
 
 
 
-def Normalization_lin(Data, FreqPointsNum, num_of_spectra):
+def Normalization_lin(data, freq_points_num, num_of_spectra):
     '''
     Normalization of amplidude-frequency responce in linear scale!!!
     Input parameters:
-        Data - input array
-        FreqPointsNum - number of frequency channels (data array dimension)
+        data - input array
+        freq_points_num - number of frequency channels (data array dimension)
         Num_of_apectra - number of spectra (data array dimension)
     Output parameters:
-        Data - result normalized data array
+        data - result normalized data array
     '''
-    DataMinfind = []
-    DataMinNorm = [0 for col in range(FreqPointsNum)]
-    for i in range (FreqPointsNum):
-        DataMinfind = Data[i, :]    # taking 1 line of matrix to find 5 minimum values
-        DataMinNorm[i] = np.mean(DataMinfind[DataMinfind.argsort()[:5]])   # calculating mean of 5 minimum values knowing their indexes
+    data_min_norm = [0 for col in range(freq_points_num)]
+    for i in range (freq_points_num):
+        data_min_findind = data[i, :]    # taking 1 line of matrix to find 5 minimum values
+        data_min_norm[i] = np.mean(data_min_findind[data_min_findind.argsort()[:5]])   # calculating mean of 5 minimum values knowing their indexes
 
     for k in range (num_of_spectra):
-        Data[:, k] = Data[:, k] / DataMinNorm[:]
-    return Data
+        data[:, k] = data[:, k] / data_min_norm[:]
+    return data
