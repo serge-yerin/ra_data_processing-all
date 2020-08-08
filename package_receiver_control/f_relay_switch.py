9b2131e7d9ff797a44249e91d2ed13e998694f7e
serge-yerin <yerin.serge@gmail.com>
2020-08-08 11:14:27 +0300

    small format improvements

--- package_receiver_control/f_relay_switch.py
+++ package_receiver_control/f_relay_switch.py
@@ -9,14 +9,16 @@ import time
 #                          M A I N    F U N C T I O N                           *
 # *******************************************************************************
 
+
 def read_relay_output(serversocket):
     byte = b'a'
     message = bytearray([])
     byte = serversocket.recv(8)
     message.extend(byte)
     message = bytes(message).decode()
-    print('\n Relays status: ', message[0:2])
+    print('\n  Relays status: ', message[0:2])
     return message
+
 
 def f_send_command_to_relay(serversocket, relay_no, command):
     '''
@@ -29,7 +31,7 @@ def f_send_command_to_relay(serversocket, relay_no, command):
     '''
     relay_no_str = str(relay_no)
     if str(command).lower() == 'on_on-off':    # Press ON button (and release it in 0.4 s)
-        print('  Pressing ON-OFF button (to switch on the computer)')
+        print('\n  Pressing ON-OFF button (to switch on the computer) on relay ', relay_no_str)
         send_command = '1' + relay_no_str
         serversocket.send(send_command.encode())
         message = read_relay_output(serversocket)
@@ -38,7 +40,7 @@ def f_send_command_to_relay(serversocket, relay_no, command):
         serversocket.send(send_command.encode())
         message = read_relay_output(serversocket)
     elif str(command).lower() == 'off_on-off':    # Press OFF button (and release it in 10 s)
-        print('  Long pressing ON-OFF button (to switch off the computer)')
+        print('\n  Long pressing ON-OFF button (to switch off the computer) on relay ', relay_no_str)
         send_command = '1' + relay_no_str
         serversocket.send(send_command.encode())
         message = read_relay_output(serversocket)
@@ -48,17 +50,18 @@ def f_send_command_to_relay(serversocket, relay_no, command):
         message = read_relay_output(serversocket)
     else:
         if str(command).lower() == 'on':        # ON and keep state
-            print('  Short (1) the relay input ', relay_no_str)
+            print('\n  Short (1) the relay input ', relay_no_str)
             command = '1'
         elif str(command).lower() == '0':       # Check state
             command = '0'
         else:                                   # OFF and keep state
-            print('  Release (0) the relay input ', relay_no_str)
+            print('\n  Release (0) the relay input ', relay_no_str)
             command = '2'
         send_command = command + relay_no_str
         serversocket.send(send_command.encode())
         message = read_relay_output(serversocket)
     return
+
 
 def f_relay_control(host, port):
     '''
