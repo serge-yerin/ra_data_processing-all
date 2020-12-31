4efc3c12281059c04621a888e7561f67d4839d72
serge-yerin <yerin.serge@gmail.com>
2021-01-01 00:54:07 +0200

    added function without token reading each time

--- package_common_modules/telegram_bot_sendtext.py
+++ package_common_modules/telegram_bot_sendtext.py
@@ -18,6 +18,22 @@ def telegram_bot_sendtext(chat_id, bot_message):
     return response.json()
 
 
+def telegram_bot_token_sendtext(chat_id, bot_token, bot_message):
+    '''
+    Sending message through telegram bot
+    Input variables:
+        chat_id - Telegram chat ID to send the message
+        bot_token - Telegram token to verify the sender
+        bot_message - string of text message to send
+    Output variables:
+        return - response of the bot with message status (json format)
+    '''
+
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+
+
 if __name__ == '__main__':
 
     chat_id = '927534685'
