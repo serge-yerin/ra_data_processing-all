b953e02af20d29dc9c3bf18f56f16134579dccae
gurt <yerin.serge@gmail.com>
2021-02-21 14:54:41 +0200

    added averaged filter

--- package_ra_data_processing/filtering.py
+++ package_ra_data_processing/filtering.py
@@ -1,8 +1,16 @@
-'''
-'''
+"""
+Simple 1D data filters
+"""
+
 from scipy import ndimage
+from scipy.ndimage.filters import uniform_filter1d
 
 
 def median_filter(data, window_len):
     fitered_data = ndimage.median_filter(data, size=window_len)
+    return fitered_data
+
+
+def average_filter(data, window_len):
+    fitered_data = uniform_filter1d(data, size=window_len)
     return fitered_data
