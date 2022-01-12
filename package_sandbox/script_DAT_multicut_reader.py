c8dc1c0cc831a34e090b09e7f1d86d3c6b51a998
serge-yerin <yerin.serge@gmail.com>
2022-01-12 17:39:53 +0200

    dat_multicat_reader is ready to use

--- package_sandbox/script_DAT_multicut_reader.py
+++ package_sandbox/script_DAT_multicut_reader.py
@@ -1,17 +1,18 @@
 # Python3
+import os
 import sys
 
-software_version = '2022.01.11'  # ######## NOT FINISHED !!! ############
-software_name = 'DAT multicut data reader'
 # Program intended to read and show data from DAT files (needs Timeline.txt as well) with several cuts in time
-import os
+software_version = '2022.01.11'
+software_name = 'DAT multicut data reader'
+
 # *******************************************************************************
 #                              P A R A M E T E R S                              *
 # *******************************************************************************
 # Path to data files
 path_to_data = 'DATA/'  # ''
 
-timeline_file_name = 'A190904_211631.adr_Timeline.txt'
+timeline_file_name = 'B210622_162805.adr_Timeline.txt'
 
 # Path to intermediate data files and results
 path_to_results = os.path.dirname(os.path.realpath(__file__)) + '/'  # Project folder or 'DATA/'
@@ -21,12 +22,10 @@ path_to_results = os.path.dirname(os.path.realpath(__file__)) + '/'  # Project f
 data_types = ['chA', 'chB', 'C_m', 'C_p', 'CRe', 'CIm']
 
 # List of frequencies to build intensity changes vs. time and save to TXT file:
-# freq_list = [10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 65.0, 70.0, 75.0]
-# freq_list = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
-freq_list = [4.0, 5.0, 6.0, 7.0, 8.0, 8.05, 8.1, 8.15, 8.5, 9.0]
+# freq_list = [4.0, 5.0, 6.0, 7.0, 8.0, 8.05, 8.1, 8.15, 8.5, 9.0]
+freq_list = []
 
 aver_or_min = 0                    # Use average value (0) per data block or minimum value (1)
-start_stop_switch = 0              # Read the whole file (0) or specified time limits (1)
 spec_freq_range = 0                # Specify particular frequency range (1) or whole range (0)
 v_min_man = -120                   # Manual lower limit of immediate spectrum figure color range
 v_max_man = -10                    # Manual upper limit of immediate spectrum figure color range
@@ -40,16 +39,15 @@ channel_save_png = 0               # Save intensities at specified frequencies t
 list_or_all_freqs = 0              # Take all frequencies of a list to save TXT and PNG? 1-All, 0-List
 amplitude_re_im = 200000 * 10**(-12)  # Color range of Re and Im dynamic spectra
                                       # 10 * 10**(-12) is typical value for CasA for interferometer of 2 GURT subarrays
-
 # Begin and end frequency of dynamic spectrum (MHz)
 freq_start = 0.0
 freq_stop = 10.0
 
 # Begin and end time of dynamic spectrum first cut ('yyyy-mm-dd hh:mm:ss')
-date_time_start = '2019-07-19 00:00:00'
-date_time_stop =  '2019-07-23 04:00:00'
-time_step_secs = 60  # step of the cuts in seconds 60 = 1 min, 3600 = 1 h, 86400 = 24 h.
-cuts_number = 2      # number of cuts to make
+date_time_start = '2021-06-22 16:28:10'
+date_time_stop =  '2021-06-22 16:30:10'
+time_step_secs = 180  # step of the cuts in seconds 60 = 1 min, 3600 = 1 h, 86400 = 24 h.
+cuts_number = 7       # number of cuts to make
 
 # Begin and end frequency of TXT files to save (MHz)
 freq_start_txt = 0.0
@@ -59,18 +57,21 @@ freq_stop_txt = 33.0
 #                     I M P O R T    L I B R A R I E S                          *
 # *******************************************************************************
 import time
-from datetime import datetime, timedelta
+from datetime import timedelta
 
-# My functions
+# My libs
 from package_ra_data_files_formats.DAT_file_reader import DAT_file_reader
 from package_common_modules.find_files_only_in_current_folder import find_files_only_in_current_folder
 from package_ra_data_files_formats.time_line_file_reader import time_line_file_reader
+from package_ra_data_files_formats.f_time_convertions import str_date_and_time_to_datetime
+
 # *******************************************************************************
 #                           M A I N    P R O G R A M                            *
 # *******************************************************************************
 
+
 print('\n\n\n\n\n\n\n\n   ****************************************************')
-print('   *   ', software_name, '  v.', software_version, '    *      (c) YeS 2019')
+print('   *   ', software_name, '  v.', software_version, '    *      (c) YeS 2022')
 print('   **************************************************** \n\n\n')
 
 start_time = time.time()
@@ -85,9 +86,6 @@ file_name_list = find_files_only_in_current_folder(path_to_data, '.txt', 0)
 
 # Find timeline files from TXT files
 timeline_file_name_list = [timeline_file_name]
-# for i in range(len(file_name_list)):
-#     if file_name_list[i].endswith('_Timeline.txt'):
-#         timeline_file_name_list.append(file_name_list[i])
 
 
 # Find original data file name from timeline file name
@@ -96,36 +94,19 @@ for i in range(len(timeline_file_name_list)):
     data_files_name_list.append(timeline_file_name_list[i][-31:-13])
 
 
-# print(' * Number of datasets (time line files) found: ', len(timeline_file_name_list), '\n')
-
 # Read the time line file and find the time limits
 time_line, dt_time_line = time_line_file_reader(path_to_data + timeline_file_name_list[0])
 
 
 print('\n\n                               Start                           End \n')
 print('  File time limits:   ', dt_time_line[0], '   ', dt_time_line[-1], '\n')
-# print('  Chosen time limits: ', dt_dateTimeStart, '        ', dt_dateTimeStop, '\n')
-
-
-date_time_start = '2019-07-19 00:00:00'
-date_time_stop =  '2019-07-23 04:00:00'
-time_step_secs = 60  # step of the cuts in seconds 60 = 1 min, 3600 = 1 h, 86400 = 24 h.
-cuts_number = 2      # number of cuts to make
-
-
-def str_date_and_time_to_datetime(str_date_time):
-    """Converts string notation of date and time to datetime object"""
-    datetime_date_time = datetime(int(str_date_time[0:4]), int(str_date_time[5:7]), int(str_date_time[8:10]),
-                                  int(str_date_time[11:13]), int(str_date_time[14:16]), int(str_date_time[17:19]), 0)
-    return datetime_date_time
 
 
 # Convert strings dates and times to datetime library objects
 dt_start = str_date_and_time_to_datetime(date_time_start)
 dt_stop = str_date_and_time_to_datetime(date_time_stop)
 dt_time_step_secs = timedelta(0, time_step_secs)
-# file_begin_time = str_date_and_time_to_datetime(dt_time_line[0])
-# file_end_time = str_date_and_time_to_datetime(dt_time_line[-1])
+
 
 # Create lists of cuts begins and ends time
 dt_start_list, dt_stop_list = [], []
@@ -133,34 +114,38 @@ for i in range(cuts_number):
     dt_start_list.append(dt_start + i * dt_time_step_secs)
     dt_stop_list.append(dt_stop + i * dt_time_step_secs)
 
+
 # Check if time of the first cut begin is after the file time begin
 if dt_start_list[0] < dt_time_line[0]:
     sys.exit('\n  ERROR! First cut start time is before the file begin time! \n')
+
 # Check if time of the last cut end is before the file time end
 if dt_stop_list[-1] > dt_time_line[-1]:
     sys.exit('\n  ERROR! First cut start time is before the file begin time! \n')
 
-# Check if the time of next cut begin is after previous cut end
+
+print('\n  Seems the time and the number of cuts are selected correctly! \n')
+print('\n\n                               Start                           End \n')
+print('  File time limits:   ', dt_time_line[0], '\n')
+for i in range(cuts_number):
+    print('  Chosen time limits: ', str(dt_start_list[i]), '          ', str(dt_stop_list[i]), '')
+print('\n  File time limits:                                  ', dt_time_line[-1], '\n\n')
 
 
-input()
+# Loop by time cuts
+for cut in range(cuts_number):
 
-# Loop by data types selected by user
-for file_no in range(len(data_files_name_list)):
-    print('   Dataset No: ', file_no + 1, ' of ', len(timeline_file_name_list), '\n')
-    dat_types_list = []
-    for type_of_data in range(len(data_types)):
-        name = data_files_name_list[file_no] + '_Data_' + data_types[type_of_data] + '.dat'
-        if os.path.isfile(path_to_data + name):
-            dat_types_list.append(data_types[type_of_data])
+    print('\n  * Cut # ', cut+1, ' of ', cuts_number,' from ', dt_start_list[cut], ' till ', dt_stop_list[cut], '\n')
 
-    result_folder_name = data_files_name_list[file_no]
+    result_folder_name = data_files_name_list[0] + '_cut_' + \
+        str(dt_start_list[cut]).replace(':', '-').replace(' ', '_') + '_-_' + \
+        str(dt_stop_list[cut]).replace(':', '-').replace(' ', '_')
 
-    done_or_not = DAT_file_reader(path_to_data, data_files_name_list[file_no], dat_types_list, '', result_folder_name,
-                                  aver_or_min, start_stop_switch, spec_freq_range, v_min_man, v_max_man, v_min_norm_man,
-                                  v_max_norm_man, rfi_mean_const, custom_dpi, colormap, channel_save_txt,
-                                  channel_save_png, list_or_all_freqs, amplitude_re_im, freq_start, freq_stop,
-                                  date_time_start, date_time_stop, freq_start_txt, freq_stop_txt, freq_list, 0)
+    done_or_not = DAT_file_reader(path_to_data, data_files_name_list[0], data_types, '', result_folder_name,
+                                  aver_or_min, 1, spec_freq_range, v_min_man, v_max_man, v_min_norm_man, v_max_norm_man,
+                                  rfi_mean_const, custom_dpi, colormap, channel_save_txt, channel_save_png,
+                                  list_or_all_freqs, amplitude_re_im, freq_start, freq_stop, str(dt_start_list[cut]),
+                                  str(dt_stop_list[cut]), freq_start_txt, freq_stop_txt, freq_list, 0)
 
 stop_time = time.time()
 print('\n\n\n  The program execution lasted for ',
