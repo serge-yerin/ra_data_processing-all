bc81fe4203cbff384193fc3a57f4c0befa6735ed
serge-yerin <yerin.serge@gmail.com>
2022-05-21 19:26:58 +0300

    mask making works

--- package_cleaning/dat_rfi_mask_making.py
+++ package_cleaning/dat_rfi_mask_making.py
@@ -1,10 +1,10 @@
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from package_ra_data_files_formats.file_header_JDS import FileHeaderReaderJDS
 from package_ra_data_processing.f_spectra_normalization import normalization_db
 from package_cleaning.f_clean_dirty_lines_for_weak_signal import clean_dirty_lines_for_weak_signal
-# Files to be analyzed:
 
 
 def dat_rfi_mask_making(filepath, spectra_to_read_per_bunch):
@@ -24,11 +24,23 @@ def dat_rfi_mask_making(filepath, spectra_to_read_per_bunch):
         [df_filepath, df_filesize, df_system_name, df_obs_place, df_description, clock_frq,
          df_creation_time_utc, sp_in_file, receiver_mode, mode, n_avr, time_resolution, fmin, fmax,
          df, frequency, freq_points_num, data_block_size] = FileHeaderReaderJDS(filepath, 0, 1)
-
-    data_file = open(filepath, 'rb')
-    data_file.seek(1024, os.SEEK_SET)  # Jumping to 1024+number of spectra to skip byte from file beginning
+    else:
+        sys.exit('Error file format')
 
     chunks_in_file = int(sp_in_file / spectra_to_read_per_bunch)
+
+    # *** Open data file and read it's header ***
+    data_file = open(filepath, 'rb')
+    # data_file.seek(1024, os.SEEK_SET)  # Jumping to 1024+number of spectra to skip byte from file beginning
+    file_header = data_file.read(1024)  # Data file header read
+
+    # *** Creating a binary file with data for long data storage ***
+    new_data_file_name = filepath[:-3] + 'msk'
+    new_data_file = open(new_data_file_name, 'wb')
+    new_data_file.write(file_header)
+    del file_header
+    new_data_file.close()
+    print(' Making mask to clean data...')
 
     for chunk in range(chunks_in_file):
 
@@ -36,9 +48,20 @@ def dat_rfi_mask_making(filepath, spectra_to_read_per_bunch):
         data = np.fromfile(data_file, dtype=np.float64, count=spectra_to_read_per_bunch * len(frequency))
         data = np.reshape(data, [len(frequency), spectra_to_read_per_bunch], order='F')
 
-        mask = clean_dirty_lines_for_weak_signal(data, 2)
+        cleaned_array, mask, dirty_points = clean_dirty_lines_for_weak_signal(data)
+
+        print('Data type of the mask: ', mask.dtype)
+
+        # temp_to_write = np.transpose(array_compensated_dm).copy(order='C')
+        new_data_file = open(new_data_file_name, 'ab')
+        mask = np.transpose(mask).copy(order='C')
+        new_data_file.write(mask)
+        new_data_file.close()
+
+    return new_data_file_name
 
 
 if __name__ == '__main__':
     filepath = 'P250322_082507.jds_Data_chA.dat'
-    dat_rfi_mask_making(filepath, 4000)
+    file_name = dat_rfi_mask_making(filepath, 4000)
+    print('Mask file name: ', file_name)
