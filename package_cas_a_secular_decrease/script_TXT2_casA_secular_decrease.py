1137388e3b982a8c4b1cb4d890eb9ecbee40ac00
serge-yerin <yerin.serge@gmail.com>
2022-06-09 22:34:14 +0300

    style corrections

--- package_cas_a_secular_decrease/script_TXT2_casA_secular_decrease.py
+++ package_cas_a_secular_decrease/script_TXT2_casA_secular_decrease.py
@@ -3,24 +3,24 @@ Software_name = 'CasA secular decrease TXT reader'
 Software_version = '2019.10.05'
 # Script intended to read, show and analyze data from
 
-#*******************************************************************************
-#                             P A R A M E T E R S                              *
-#*******************************************************************************
+# *******************************************************************************
+#                              P A R A M E T E R S                              *
+# *******************************************************************************
 # Path to data files
 
-path_to_data =  'DATA/'
+path_to_data = 'DATA/'
 
 y_auto = 1
 Vmin = -100 * 10**(-12)
 Vmax =  100 * 10**(-12)
-interferometer_base = 400 #900
+interferometer_base = 400  # 900
 zero_for_log = 0.0000000000001
 customDPI = 300                     # Resolution of images of dynamic spectra
 
-################################################################################
-#*******************************************************************************
-#                    I M P O R T    L I B R A R I E S                          *
-#*******************************************************************************
+# ###############################################################################
+# *******************************************************************************
+#                     I M P O R T    L I B R A R I E S                          *
+# *******************************************************************************
 c = 2.997 * 10**8
 
 import os
@@ -40,28 +40,28 @@ if __package__ is None:
 from package_common_modules.find_files_only_in_current_folder import find_files_only_in_current_folder
 from package_common_modules.text_manipulations import find_between, read_date_time_and_one_value_txt
 from package_common_modules.find_subfolders_in_folder import find_subfolders_in_folder
-################################################################################
-#*******************************************************************************
-#                          M A I N    P R O G R A M                            *
-#*******************************************************************************
-print ('\n\n\n\n\n\n\n\n   *********************************************************************')
-print ('   *        ', Software_name, '  v.', Software_version,'         *      (c) YeS 2019')
-print ('   ********************************************************************* \n\n\n')
+
+# *******************************************************************************
+#                           M A I N    P R O G R A M                            *
+# *******************************************************************************
+print('\n\n\n\n\n\n\n\n   *********************************************************************')
+print('   *        ', Software_name, '  v.', Software_version, '         *      (c) YeS 2019')
+print('   ********************************************************************* \n\n\n')
 
 startTime = time.time()
 currentTime = time.strftime("%H:%M:%S")
 currentDate = time.strftime("%d.%m.%Y")
-print ('  Today is ', currentDate, ' time is ', currentTime)
+print('  Today is ', currentDate, ' time is ', currentTime)
 
 # Searching of the directories with needed data in predefined directory
 
-folder_list =  find_subfolders_in_folder(path_to_data, 0)
+folder_list = find_subfolders_in_folder(path_to_data, 0)
 
 # Excluding the folders that are in subfolders of current folder
 only_subfolder_list = []
 for i in range(len(folder_list)):
     temp = folder_list[i]
-    if ('/' not in temp.replace(path_to_data,'')) and ('\\' not in temp.replace(path_to_data,'')):
+    if ('/' not in temp.replace(path_to_data, '')) and ('\\' not in temp.replace(path_to_data, '')):
         only_subfolder_list.append(folder_list[i])
 
 
@@ -104,14 +104,14 @@ date_of_experiment_spectra = ''
 
 for source in ['SygA', 'CasA']:
 
-    if source == 'SygA': path_to_data = path_to_data_SygA
-    if source == 'CasA': path_to_data = path_to_data_CasA
+    if source == 'SygA':
+        path_to_data = path_to_data_SygA
+    if source == 'CasA':
+        path_to_data = path_to_data_CasA
 
-
-    # Finding TXT files with interferometric responces in the folder
+    # Finding TXT files with interferometric responses in the folder
     print('')
     file_name_list = find_files_only_in_current_folder(path_to_data, '.txt', 1)
-
 
     # Reading description of the observation from INFO file
     info_txt_file = find_files_only_in_current_folder(path_to_data, '.info', 0)
@@ -120,8 +120,10 @@ for source in ['SygA', 'CasA']:
         file_text.append(line)
         if line.startswith(" Description:"):  # Searching comments
             words_in_line = line.split()
-            if source == 'SygA': description_SygA = words_in_line[1]    # reading description of data file
-            if source == 'CasA': description_CasA = words_in_line[1]    # reading description of data file
+            if source == 'SygA':
+                description_SygA = words_in_line[1]    # reading description of data file
+            if source == 'CasA':
+                description_CasA = words_in_line[1]    # reading description of data file
         if line.startswith(" Culmination"):  # Searching comments
             words_in_line = line.split()
             if source == 'SygA':
@@ -129,8 +131,7 @@ for source in ['SygA', 'CasA']:
     TXT_file.close()
 
     # Loop by found TXT files in the folder
-    for file_no in range (len(file_name_list)):
-
+    for file_no in range(len(file_name_list)):
 
         if '.jds' in file_name_list[file_no]:
             data_type = find_between(file_name_list[file_no], '_', '.jds')[-3:]
@@ -138,7 +139,7 @@ for source in ['SygA', 'CasA']:
             data_type = find_between(file_name_list[file_no], '_', '.adr')[-3:]
 
         # *** Reading files ***
-        [x_value, y_value] = read_date_time_and_one_value_txt ([path_to_data + file_name_list[file_no]])
+        [x_value, y_value] = read_date_time_and_one_value_txt([path_to_data + file_name_list[file_no]])
 
         y_value = np.array(y_value)
         a, b = y_value.shape
@@ -153,15 +154,13 @@ for source in ['SygA', 'CasA']:
 
         parent_filename = find_between(path_to_data + file_name_list[0], path_to_data, ' variation')
 
-        x_center = int(b/2) # Central point in time axis (culmination time usually)
+        x_center = int(b/2)  # Central point in time axis (culmination time usually)
 
         amplitude = (np.max(y_value[0, :]) + np.abs(np.min(y_value[0, :])))/2
 
-
-
-        #*******************************************************************************
-        #                                F I G U R E S                                 *
-        #*******************************************************************************
+        # *******************************************************************************
+        #                                 F I G U R E S                                 *
+        # *******************************************************************************
         '''
         rc('font', size = 6, weight='bold')
         fig = plt.figure(figsize = (9, 5))
@@ -200,32 +199,31 @@ for source in ['SygA', 'CasA']:
         ymax = np.max(ampl_spectra)
         max_index = np.argmax(ampl_spectra[0:50])
 
-
         # Filtering of data
         if   max_index <= 2:
-            experiment_spectra[max_index + 3 :] = zero_for_log
+            experiment_spectra[max_index + 3:] = zero_for_log
         else:
-            experiment_spectra[max_index + 3 : ] = zero_for_log
-            experiment_spectra[ : max_index - 2] = zero_for_log
+            experiment_spectra[max_index + 3: ] = zero_for_log
+            experiment_spectra[: max_index - 2] = zero_for_log
 
-        # Making inverse FFT to obtain the cleaned responce
+        # Making inverse FFT to obtain the cleaned response
         filtered_signal_re = np.real(np.fft.ifft(experiment_spectra))
 
-        # Finding the maximal absolute valeu of the responce
+        # Finding the maximal absolute value of the response
         ymax = 2 * np.max(np.abs(filtered_signal_re))
 
         # Figure of the initial and filtered interferometric responce
-        rc('font', size = 6, weight='bold')
-        fig = plt.figure(figsize = (9, 5))
-        fig.suptitle('Interferometric responce only 5 harmonics', fontsize = 8, fontweight='bold')
+        rc('font', size=6, weight='bold')
+        fig = plt.figure(figsize=(9, 5))
+        fig.suptitle('Interferometric response only 5 harmonics', fontsize = 8, fontweight='bold')
         ax1 = fig.add_subplot(111)
-        plt.axvline(x = x_center, linewidth = '0.5' , color = 'r') #, alpha=0.5
-        plt.axhline(y =  ymax, linewidth = '1.5' , color = 'r') #, alpha=0.5
-        plt.axhline(y = -ymax, linewidth = '1.5' , color = 'r') #, alpha=0.5
-        ax1.plot(2 * filtered_signal_re, linestyle = '-', linewidth = '1.00', label = 'Filtered')
-        ax1.plot(y_value[0, :], linestyle = '-', linewidth = '1.00', label = 'Measured')
-        ax1.legend(loc = 'upper right', fontsize = 6)
-        ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+        plt.axvline(x=x_center, linewidth='0.5', color='r')  # alpha=0.5
+        plt.axhline(y=ymax, linewidth='1.5', color='r')  # alpha=0.5
+        plt.axhline(y=-ymax, linewidth='1.5', color='r')  # alpha=0.5
+        ax1.plot(2 * filtered_signal_re, linestyle='-', linewidth='1.00', label='Filtered')
+        ax1.plot(y_value[0, :], linestyle='-', linewidth='1.00', label='Measured')
+        ax1.legend(loc='upper right', fontsize=6)
+        ax1.grid(b=True, which='both', color='silver', linestyle='-')
         ax1.set_ylim([Vmin, Vmax])
         ax1.set_xlim([0, b-1])
         ax1.set_ylabel('Intensity, a.u.', fontsize=6, fontweight='bold')
@@ -235,18 +233,20 @@ for source in ['SygA', 'CasA']:
         for i in range(len(text)):
             k = int(text[i])
             text[i] = str(date_time[k][0:11] + '\n' + date_time[k][11:23])
-        ax1.set_xticklabels(text, fontsize = 6, fontweight = 'bold')
+        ax1.set_xticklabels(text, fontsize=6, fontweight='bold')
         fig.subplots_adjust(top=0.92)
-        pylab.savefig(result_path + '/' + parent_filename +' Interferometric responce '+source+' '+data_type+' '+text_freq+' MHz.png', bbox_inches = 'tight', dpi = 160)
+        pylab.savefig(result_path + '/' + parent_filename + ' Interferometric responce ' + source + ' ' +
+                      data_type + ' ' + text_freq + ' MHz.png', bbox_inches='tight', dpi=160)
         plt.close('all')
 
-
-
-        if data_type == 'CIm' and source == 'SygA': ampl_list_SygA_CIm.append(ymax)
-        if data_type == 'CRe' and source == 'SygA': ampl_list_SygA_CRe.append(ymax)
-        if data_type == 'CIm' and source == 'CasA': ampl_list_CasA_CIm.append(ymax)
-        if data_type == 'CRe' and source == 'CasA': ampl_list_CasA_CRe.append(ymax)
-
+        if data_type == 'CIm' and source == 'SygA':
+            ampl_list_SygA_CIm.append(ymax)
+        if data_type == 'CRe' and source == 'SygA':
+            ampl_list_SygA_CRe.append(ymax)
+        if data_type == 'CIm' and source == 'CasA':
+            ampl_list_CasA_CIm.append(ymax)
+        if data_type == 'CRe' and source == 'CasA':
+            ampl_list_CasA_CRe.append(ymax)
 
         '''
         rc('font', size = 6, weight='bold')
@@ -272,10 +272,8 @@ for source in ['SygA', 'CasA']:
 
 
 if freq_list_SygA_CRe != freq_list_CasA_CRe:
-    print ('   ERROR! Frequencies of analysis for two sources do not match!!! \n\n')
+    print('   ERROR! Frequencies of analysis for two sources do not match!!! \n\n')
     sys.exit('           Program stopped! \n\n')
-
-
 
 # Calculating the flux ratio
 flux_ratio_CRe = []
@@ -284,12 +282,11 @@ for i in range(len(ampl_list_CasA_CRe)):
     flux_ratio_CIm.append(ampl_list_CasA_CIm[i] / ampl_list_SygA_CIm[i])
     flux_ratio_CRe.append(ampl_list_CasA_CRe[i] / ampl_list_SygA_CRe[i])
 
-
-
 # Printing to terminal the calculated ratios
 print('\n Frequency, MHz  |  Flux ratio Re  |  Flux ratio Im')
-for i in range (len(freq_list_SygA_CRe)):
-    print('   ', freq_list_SygA_CRe[i], '          ', ''.join(format(np.round(flux_ratio_CRe[i], 5), "8.5f")), '        ', ''.join(format(np.round(flux_ratio_CIm[i], 5), "8.5f")))
+for i in range(len(freq_list_SygA_CRe)):
+    print('   ', freq_list_SygA_CRe[i], '          ', ''.join(format(np.round(flux_ratio_CRe[i], 5), "8.5f")),
+          '        ', ''.join(format(np.round(flux_ratio_CIm[i], 5), "8.5f")))
 
 # Making string of observations start date
 if date_of_experiment_spectra == '':
@@ -299,49 +296,56 @@ if date_of_experiment_spectra == '':
     if '.adr' in file_name_list[file_no]:
         date_of_experiment_spectra = '20' + file_name_SygA[1:3] + '.' + file_name_SygA[3:5] + '.' + file_name_SygA[5:7]
 
-date_of_experiment_spectra = date_of_experiment_spectra.replace('-','.')
+date_of_experiment_spectra = date_of_experiment_spectra.replace('-', '.')
 
 
 # Making a figure of flux ratio with frequency
-#'''
-rc('font', size = 6, weight='bold')
-fig = plt.figure(figsize = (10, 5))
-fig.suptitle('Flux ratio of Cas A and Syg A with frequency', fontsize = 8, fontweight='bold')
+# '''
+rc('font', size=6, weight='bold')
+fig = plt.figure(figsize=(10, 5))
+fig.suptitle('Flux ratio of Cas A and Syg A with frequency', fontsize=8, fontweight='bold')
 ax1 = fig.add_subplot(111)
-ax1.set_title('Ratio of interferometric responces for files: ' + file_name_SygA + ' and ' + file_name_CasA + '\n Syg A description: '+ description_SygA + ', Cas A description: '+ description_CasA, fontsize = 7)
-ax1.plot(freq_list_SygA_CRe, flux_ratio_CRe, 'ro',  label = 'Real part of correlation')
-ax1.plot(freq_list_SygA_CIm, flux_ratio_CIm, 'bo', alpha = 0.7, label = 'Imag part of correlation')
-#for i in range (len(freq_list_SygA)):
+ax1.set_title('Ratio of interferometric responses for files: ' + file_name_SygA + ' and ' + file_name_CasA +
+              '\n Syg A description: ' + description_SygA + ', Cas A description: ' + description_CasA, fontsize=7)
+ax1.plot(freq_list_SygA_CRe, flux_ratio_CRe, 'ro',  label='Real part of correlation')
+ax1.plot(freq_list_SygA_CIm, flux_ratio_CIm, 'bo', alpha=0.7, label='Imag part of correlation')
+# for i in range (len(freq_list_SygA)):
 #    ax1.annotate(str(np.round(flux_ratio[i],3)),  xy=(freq_list_SygA[i], flux_ratio[i]+0.1), fontsize = 6, ha='center')
 if 'jds' in file_name_SygA: ax1.set_xlim([6, 34])
 if 'adr' in file_name_SygA: ax1.set_xlim([8, 80])
 ax1.set_ylim([0, 2])
-ax1.legend(loc = 'upper left')
-ax1.grid(b = True, which = 'both', color = 'silver', linestyle = '-')
+ax1.legend(loc='upper left')
+ax1.grid(b=True, which='both', color='silver', linestyle='-')
 ax1.set_xlabel('Frequency, MHz', fontsize=6, fontweight='bold')
 ax1.set_ylabel('Flux ratio', fontsize=6, fontweight='bold')
-if 'jds' in file_name_SygA: plt.xticks([8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32])
-if 'adr' in file_name_SygA: plt.xticks([10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75])
-fig.text(0.09, 0.95, date_of_experiment_spectra, fontsize = 8, fontweight='bold', transform=plt.gcf().transFigure)
-fig.text(0.79, 0.03, 'Processed '+currentDate+ ' at '+currentTime, fontsize=4, transform=plt.gcf().transFigure)
-fig.text(0.09, 0.03, 'Software version: '+Software_version+', yerin.serge@gmail.com, IRA NASU', fontsize=4, transform=plt.gcf().transFigure)
-pylab.savefig(result_path + '/' + date_of_experiment_spectra + ' Flux ratio with frequency for files '+file_name_SygA+'_'+file_name_CasA+'.png', bbox_inches = 'tight', dpi = 160)
+if 'jds' in file_name_SygA:
+    plt.xticks([8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32])
+if 'adr' in file_name_SygA:
+    plt.xticks([10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75])
+fig.text(0.09, 0.95, date_of_experiment_spectra, fontsize=8, fontweight='bold', transform=plt.gcf().transFigure)
+fig.text(0.79, 0.03, 'Processed '+currentDate + ' at ' + currentTime, fontsize=4, transform=plt.gcf().transFigure)
+fig.text(0.09, 0.03, 'Software version: '+Software_version+', yerin.serge@gmail.com, IRA NASU', fontsize=4,
+         transform=plt.gcf().transFigure)
+pylab.savefig(result_path + '/' + date_of_experiment_spectra + ' Flux ratio with frequency for files ' +
+              file_name_SygA + '_' + file_name_CasA + '.png', bbox_inches='tight', dpi=160)
 plt.close('all')
-#'''
+# '''
 
 # Saving data on flux ratio to TXT file
-TXT_file = open(result_path + '/' + date_of_experiment_spectra + ' Flux ratio with frequency for files '+file_name_SygA+'_'+file_name_CasA+'.txt', "w")
-TXT_file.write('#  Ratio of interferometric responces of Cas A and Syg A with frequency \n#\n')
-for i in range (len(file_text)):
+TXT_file = open(result_path + '/' + date_of_experiment_spectra + ' Flux ratio with frequency for files ' +
+                file_name_SygA + '_' + file_name_CasA + '.txt', "w")
+TXT_file.write('#  Ratio of interferometric responses of Cas A and Syg A with frequency \n#\n')
+for i in range(len(file_text)):
     TXT_file.write('# ' + file_text[i])
 TXT_file.write('# \n# Frequency, MHz | Flux ratio Re | Flux ratio Im \n')
 for i in range(len(freq_list_SygA_CRe)):
-    TXT_file.write('     ' + str(freq_list_SygA_CRe[i]) + '          ' +''.join(format(np.round(flux_ratio_CRe[i], 5), "8.5f")) + '        ' + ''.join(format(np.round(flux_ratio_CIm[i], 5), "8.5f")) + '\n')
+    TXT_file.write('     ' + str(freq_list_SygA_CRe[i]) + '          ' +
+                   ''.join(format(np.round(flux_ratio_CRe[i], 5), "8.5f")) + '        ' +
+                   ''.join(format(np.round(flux_ratio_CIm[i], 5), "8.5f")) + '\n')
 TXT_file.close()
 
 
-
 endTime = time.time()
-print ('\n\n\n  The program execution lasted for ', round((endTime - startTime), 2), 'seconds (',
-                                                round((endTime - startTime)/60, 2), 'min. ) \n')
-print ('\n           *** Program ' + Software_name + ' has finished! *** \n\n\n')
+print('\n\n\n  The program execution lasted for ', round((endTime - startTime), 2), 'seconds (',
+                                                   round((endTime - startTime)/60, 2), 'min. ) \n')
+print('\n           *** Program ' + Software_name + ' has finished! *** \n\n\n')
