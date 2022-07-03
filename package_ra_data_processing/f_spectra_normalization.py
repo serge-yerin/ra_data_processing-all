39aeaf2f57af7cb7890da138694c52dbeb86d989
serge-yerin <yerin.serge@gmail.com>
2022-07-03 20:09:50 +0300

    grammar check

--- package_ra_data_processing/f_spectra_normalization.py
+++ package_ra_data_processing/f_spectra_normalization.py
@@ -4,19 +4,18 @@ import numpy as np
 
 def normalization_db(data, freq_points_num, num_of_spectra):
     """
-    Normalizing amplitude-frequency responce (spectra) in dB!!!
+    Normalizing amplitude-frequency response (spectra) in dB!!!
     Input parameters:
         data - input array
-        freq_points_num - number of frequency channels (data array dimension)
-        Num_of_apectra - number of spectra (data array dimension)
+        freq_points_num - number of frequency channels (data array dimension array.shape[1])
+        num_of_spectra - number of spectra (data array dimension array.shape[0])
     Output parameters:
         data - result normalized data array
     """
-    # data_min_norm = [0 for col in range(freq_points_num)]
     data_min_norm = np.zeros(freq_points_num)
     for j in range(freq_points_num):
         data_min_find = data[:, j]  # taking 1 line of matrix to find 5 minimum values
-        # calculating mean of 5 minimum values knowing their indexes
+        # calculating mean of 5 minimal values knowing their indexes
         data_min_norm[j] = np.mean(data_min_find[data_min_find.argsort()[:5]])
 
     for k in range(num_of_spectra):
@@ -31,15 +30,14 @@ def normalization_lin(data, freq_points_num, num_of_spectra):
     Input parameters:
         data - input array
         freq_points_num - number of frequency channels (data array dimension)
-        Num_of_apectra - number of spectra (data array dimension)
+        Num_of_spectra - number of spectra (data array dimension)
     Output parameters:
         data - result normalized data array
     """
-    # data_min_norm = [0 for col in range(freq_points_num)]
     data_min_norm = np.zeros(freq_points_num)
     for i in range(freq_points_num):
         data_min_findind = data[i, :]    # taking 1 line of matrix to find 5 minimum values
-        # calculating mean of 5 minimum values knowing their indexes
+        # calculating mean of 5 minimal values knowing their indexes
         data_min_norm[i] = np.mean(data_min_findind[data_min_findind.argsort()[:5]])
 
     for k in range(num_of_spectra):
