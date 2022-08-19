b021dc47a29e28875d050ec4820071e34cb98994
serge-yerin <yerin.serge@gmail.com>
2022-08-19 17:45:58 +0300

    added 1e-16 instead of zero and float64 data type

--- package_ra_data_processing/f_spectra_normalization.py
+++ package_ra_data_processing/f_spectra_normalization.py
@@ -12,7 +12,7 @@ def normalization_db(data, freq_points_num, num_of_spectra):
     Output parameters:
         data - result normalized data array
     """
-    data_min_norm = np.zeros(freq_points_num)
+    data_min_norm = np.zeros(freq_points_num, dtype=np.float64)
     for j in range(freq_points_num):
         data_min_find = data[:, j]  # taking 1 line of matrix to find 5 minimum values
         # calculating mean of 5 minimal values knowing their indexes
@@ -34,11 +34,13 @@ def normalization_lin(data, freq_points_num, num_of_spectra):
     Output parameters:
         data - result normalized data array
     """
-    data_min_norm = np.zeros(freq_points_num)
+    data_min_norm = np.zeros(freq_points_num, dtype=np.float64)
     for i in range(freq_points_num):
         data_min_findind = data[i, :]    # taking 1 line of matrix to find 5 minimum values
         # calculating mean of 5 minimal values knowing their indexes
         data_min_norm[i] = np.mean(data_min_findind[data_min_findind.argsort()[:5]])
+
+    data_min_norm[data_min_norm == 0] = 1e-16
 
     for k in range(num_of_spectra):
         data[:, k] = data[:, k] / data_min_norm[:]
