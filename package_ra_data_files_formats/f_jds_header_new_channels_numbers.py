96b8ee9a73993d4272df54edc008b8b1286dfad0
YeS <yerin.serge@gmail.com>
2022-09-08 08:49:23 +0300

    added fft size and averaging coefficient

--- package_ra_data_files_formats/f_jds_header_new_channels_numbers.py
+++ package_ra_data_files_formats/f_jds_header_new_channels_numbers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def jds_header_new_channels_numbers(file_header, ifmin, ifmax):
+def jds_header_new_channels_numbers(file_header, ifmin, ifmax, fft_size=None, n_avr=None):
     """
     returns .jds file header with correctly stored numbers of frequency channels
     usually is useful when while processing the last 4 channels where time info is stored are deleted
@@ -13,8 +13,12 @@ def jds_header_new_channels_numbers(file_header, ifmin, ifmax):
     """
 
     file_header = bytearray(file_header)
+    if fft_size is not None:
+        file_header[574:578] = np.int32(fft_size).tobytes()  # FFT size place in header
     file_header[624:628] = np.int32(ifmin).tobytes()
     file_header[628:632] = np.int32(ifmax).tobytes()
     file_header[632:636] = np.int32(ifmax - ifmin).tobytes()
+    if n_avr is not None:
+        file_header[636:640] = np.int32(n_avr).tobytes()  # n_avr place in header
     file_header = bytes(file_header)
     return file_header
