d747ae3a0f50c13a0c64a8b0db25040eafa6b952
YeS <yerin.serge@gmail.com>
2022-10-27 21:32:45 +0300

    style corrections

--- package_pulsar_processing/pulsar_DM_variation.py
+++ package_pulsar_processing/pulsar_DM_variation.py
@@ -1,49 +1,46 @@
-'''
-'''
 
 import numpy as np
 from package_pulsar_processing.pulsar_DM_compensation_with_indices_changes import pulsar_DM_compensation_with_indices_changes
 from package_pulsar_processing.pulsar_dm_shift_calculation_aver_pulse import pulsar_dm_shift_calculation_aver_pulse
 
-def pulsar_DM_variation(array, no_of_DM_steps, FFTsize, fmin, fmax, df, TimeRes, pulsarPeriod, samplesPerPeriod, DM, noise_mean, noise_std, begin_index, end_index, DM_var_step, roll_number, save_intermediate_data, customDPI):
-    '''
+
+def pulsar_DM_variation(array, no_of_dm_steps, fft_size, fmin, fmax, df, time_res, pulsar_period, samples_per_period,
+                        pulsar_dm,  noise_mean, noise_std, begin_index, end_index, dm_var_step, roll_number):
+    """
     Variation of DM for average pulsar profile with defined step and number of steps
-    '''
+    """
     # ***   Preparing matrices for variation of DM value   ***
-    profiles_varDM = np.zeros((no_of_DM_steps, samplesPerPeriod))
-    DM_vector = np.zeros((no_of_DM_steps))
+    profiles_var_dm = np.zeros((no_of_dm_steps, samples_per_period))
+    dm_vector = np.zeros((no_of_dm_steps))
 
-
-    # ***   Preparing the vector of DMs to calculate profiles   ***
-    for step in range (no_of_DM_steps):
-        DM_vector[step] = DM + ((step - int(no_of_DM_steps/2)) * DM_var_step)
+    #  Preparing the vector of DMs to calculate profiles
+    for step in range(no_of_dm_steps):
+        dm_vector[step] = pulsar_dm + ((step - int(no_of_dm_steps/2)) * dm_var_step)
 
     # ***   Step by step calculate profiles for each DM value as in the main program   ***
-    for step in range (no_of_DM_steps):
+    for step in range(no_of_dm_steps):
 
         # Preparing matrices
-        matrix = np.zeros((FFTsize, samplesPerPeriod))
-        matrix[:,:] = array[:,:]
+        matrix = np.zeros((fft_size, samples_per_period))
+        matrix[:, :] = array[:, :]
 
         # DM compensation
-        shiftPar = pulsar_dm_shift_calculation_aver_pulse(FFTsize, fmin, fmax, df, TimeRes, DM_vector[step], pulsarPeriod)
-        matrix = pulsar_DM_compensation_with_indices_changes (matrix, shiftPar)
+        shift_param = pulsar_dm_shift_calculation_aver_pulse(fft_size, fmin, fmax, df, time_res,
+                                                             dm_vector[step], pulsar_period)
+        matrix = pulsar_DM_compensation_with_indices_changes(matrix, shift_param)
 
-        for i in range (FFTsize):
-            matrix[i,:] = matrix[i,:] - np.mean(matrix[i, begin_index : end_index])
+        for i in range(fft_size):
+            matrix[i, :] = matrix[i, :] - np.mean(matrix[i, begin_index: end_index])
 
-        # Calcultation of integrated profile
-        integrated_profile = np.sum(matrix, axis = 0)
-
+        # Calculation of integrated profile
+        integrated_profile = np.sum(matrix, axis=0)
 
         integrated_profile = (integrated_profile - noise_mean) / noise_std
 
-        # Adding the calcultaed average profile to the matrix of profiles
-        profiles_varDM[step, :] = integrated_profile
+        # Adding the calculated average profile to the matrix of profiles
+        profiles_var_dm[step, :] = integrated_profile
 
+    #  Rolling to central position of pulse the whole matrix
+    profiles_var_dm = np.roll(profiles_var_dm, roll_number)
 
-    # ***   Rolling to central position of pulse the whole matrix   ***
-    profiles_varDM = np.roll(profiles_varDM, roll_number)
-
-
-    return profiles_varDM, DM_vector
+    return profiles_var_dm, dm_vector
