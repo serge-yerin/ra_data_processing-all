3be333e9f547237254fae4c60e123675661f08c5
YeS <yerin.serge@gmail.com>
2022-11-02 20:49:41 +0200

    style corrections

--- package_pulsar_processing/pulsar_DM_compensation_with_indices_changes.py
+++ package_pulsar_processing/pulsar_DM_compensation_with_indices_changes.py
@@ -1,10 +1,9 @@
-'''
-'''
 import numpy as np
+
+
 def pulsar_DM_compensation_with_indices_changes (array, shift_vector):
 
     rows, column_indices = np.ogrid[:array.shape[0], :array.shape[1]]
-    #result = np.zeros_like(array)
 
     # Use always a negative shift, so that column_indices are valid (could also use module operation).
     shift_vector[shift_vector < 0] += array.shape[1]
