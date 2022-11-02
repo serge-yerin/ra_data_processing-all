44b4e013eb85e7ae05d5d3ff645c82a8b83a11db
YeS <yerin.serge@gmail.com>
2022-11-02 20:50:02 +0200

    style corrections

--- package_pulsar_processing/pulsar_DM_compensation_roll.py
+++ package_pulsar_processing/pulsar_DM_compensation_roll.py
@@ -1,21 +1,33 @@
-'''
-'''
-
 import numpy as np
 import math
-from f_plot_formats import plot1D
 
 
-def pulsar_DM_compensation_roll(matrix_0, freq_points, fmin, fmax, df, TimeRes, pulsarPeriod, DM0, save_intermediate_data, customDPI):
-    '''
-    Plots 1D plots of variable
-    '''
-    dt = np.zeros((freq_points), dtype = 'float')                # time of shifting channel to dedisperse
-    for i in range (freq_points):
-        dt[i] = (DM0 / 2.410331) * ((10000./math.pow((fmin + df * i), 2)) - (10000./math.pow(fmax, 2)))
-    shiftPar = np.zeros((freq_points), dtype = 'int')            # number of steps to shift each row
-    for i in range (freq_points):
-        shiftPar[i] = -1 * round(pulsarPeriod * (dt[i] / pulsarPeriod - math.floor(dt[i] / pulsarPeriod)) / TimeRes)
-        matrix_0[i] = np.roll(matrix_0[i], shiftPar[i])
+def pulsar_DM_compensation_roll(data_array, freq_points, fmin, fmax, df, time_res, pulsar_period, pulsar_dm):
 
-    return matrix_0, shiftPar
+    """
+    Function compensates dispersion delay with rolling each row of the array
+    Args:
+        data_array: array of data to compensate dispersion delay (np.array)
+        freq_points: a list of frequency points (np.float)
+        fmin: minimal frequency (float)
+        fmax: maximal frequency (float)
+        df: frequency resolution (float)
+        time_res: time resolution (float)
+        pulsar_period: pulsar period (np.float64)
+        pulsar_dm: pulsar dispersion measure (np.float)
+
+    Returns:
+        data_array: data array with compensated dispersion delay
+        shift_parameter: a vector of shift numbers for each array row
+    """
+
+    time_shift = np.zeros((freq_points), dtype='float')  # time of shifting channel to dedisperse
+    for i in range(freq_points):
+        time_shift[i] = (pulsar_dm / 2.410331) * ((10000./math.pow((fmin + df * i), 2)) - (10000./math.pow(fmax, 2)))
+    shift_parameter = np.zeros((freq_points), dtype='int')  # number of steps to shift each row
+    for i in range(freq_points):
+        shift_parameter[i] = -1 * round(pulsar_period * (time_shift[i] / pulsar_period -
+                                                         math.floor(time_shift[i] / pulsar_period)) / time_res)
+        data_array[i] = np.roll(data_array[i], shift_parameter[i])
+
+    return data_array, shift_parameter
