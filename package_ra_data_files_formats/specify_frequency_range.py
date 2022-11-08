45bf96b17a2e0a5bf13f40aaaea4f67683d235ad
YeS <yerin.serge@gmail.com>
2022-11-08 23:11:07 +0200

    style corrections

--- package_ra_data_files_formats/specify_frequency_range.py
+++ package_ra_data_files_formats/specify_frequency_range.py
@@ -1,30 +1,31 @@
-'''
-'''
-
 ################################################################################
 
 def specify_frequency_range(array, frequency, freq_start, freq_stop):
-    '''
+    """
     The function cuts array to necessary frequency range
-    Input parameters:
+    Args:
+        array: 
+        frequency: 
+        freq_start: 
+        freq_stop: 
 
-    Output parameters:
-
-    '''
-    print ('\n You have chosen the frequency range', round(freq_start,6), '-', round(freq_stop,6), 'MHz')
-    A = []
-    B = []
-    for i in range (len(frequency)):
-        A.append(abs(frequency[i] - freq_start))
-        B.append(abs(frequency[i] - freq_stop))
-    ifmin = A.index(min(A))
-    ifmax = B.index(min(B))
-    new_array = array[ifmin : ifmax, :]
-    print ('\n New data array shape is: ', array.shape)
-    frequency = frequency[ifmin : ifmax]
+    Returns:
+        new_array:
+        frequency:
+        ifmin:
+        ifmax:
+    """
+    print('\n You have chosen the frequency range', round(freq_start, 6), '-', round(freq_stop, 6), 'MHz')
+    tmp_list_a = []
+    tmp_list_b = []
+    for i in range(len(frequency)):
+        tmp_list_a.append(abs(frequency[i] - freq_start))
+        tmp_list_b.append(abs(frequency[i] - freq_stop))
+    ifmin = tmp_list_a.index(min(tmp_list_a))
+    ifmax = tmp_list_b.index(min(tmp_list_b))
+    del tmp_list_a, tmp_list_b
+    new_array = array[ifmin: ifmax, :]
+    print('\n New data array shape is: ', array.shape)
+    frequency = frequency[ifmin: ifmax]
 
     return new_array, frequency, ifmin, ifmax
-
-################################################################################
-
-################################################################################
