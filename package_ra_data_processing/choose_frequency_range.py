1439ece52be700667a6bcd958156620bdf117c95
YeS <yerin.serge@gmail.com>
2022-11-08 23:23:44 +0200

    style corrections

--- package_ra_data_processing/choose_frequency_range.py
+++ package_ra_data_processing/choose_frequency_range.py
@@ -1,32 +1,45 @@
-'''
-'''
 
-def choose_frequency_range (frequencyList, matrix, freqStart, freqStop, FFTsize, fmin, fmax):
-    '''
+
+def choose_frequency_range(frequency_list, data_array, freq_start, freq_stop, fft_size, fmin, fmax):
+    """
     Cut from array of data the range of needed frequencies
-    '''
-    num_freq = len(frequencyList)
-    if (frequencyList[0]<freqStart<frequencyList[num_freq-1]) and (frequencyList[0]<freqStop<frequencyList[num_freq-1]) and (freqStart<freqStop):
-        print ('\n  * You have chosen the frequency range', freqStart, '-', freqStop, 'MHz')
-        A = []
-        B = []
-        for i in range (len(frequencyList)):
-            A.append(abs(frequencyList[i] - freqStart))
-            B.append(abs(frequencyList[i] - freqStop))
-        ifmin = A.index(min(A))
-        ifmax = B.index(min(B))
-        matrix = matrix[ifmin:ifmax, :]  # Reshaping matrix
-        print ('\n    New data matrix shape is: ', matrix.shape)
+
+    Args:
+        frequency_list:
+        data_array:
+        freq_start:
+        freq_stop:
+        fft_size:
+        fmin:
+        fmax:
+
+    Returns:
+
+    """
+    if (frequency_list[0] < freq_start < frequency_list[-1]) and \
+            (frequency_list[0] < freq_stop < frequency_list[-1]) and (freq_start < freq_stop):
+
+        print('\n  * You have chosen the frequency range', freq_start, '-', freq_stop, 'MHz')
+
+        tmp_list_a = []
+        tmp_list_b = []
+        for i in range(len(frequency_list)):
+            tmp_list_a.append(abs(frequency_list[i] - freq_start))
+            tmp_list_b.append(abs(frequency_list[i] - freq_stop))
+        ifmin = tmp_list_a.index(min(tmp_list_a))
+        ifmax = tmp_list_b.index(min(tmp_list_b))
+        data_array = data_array[ifmin: ifmax, :]  # Reshaping array
+
+        print('\n    New data array shape is: ', data_array.shape)
 
         # Changing other data for further processing
-        freqLine = frequencyList[ifmin:ifmax]
-        frequencyList = freqLine
-        FFTsize = len(frequencyList)
-        fmin = frequencyList[0]
-        fmax = frequencyList[FFTsize-1]
+        frequency_list = frequency_list[ifmin:ifmax]
+        fft_size = len(frequency_list)
+        fmin = frequency_list[0]
+        fmax = frequency_list[fft_size-1]
 
-        del A, B, freqLine, ifmin, ifmax, num_freq
+        del tmp_list_a, tmp_list_b, ifmin, ifmax
     else:
-        print ('  !!! Error of frequency limits !!! \n    Continue processing the whole array')
+        print('  !!! Error of frequency limits !!! \n    Continue processing the whole array')
 
-    return frequencyList, matrix, FFTsize, fmin, fmax
+    return frequency_list, data_array, fft_size, fmin, fmax
