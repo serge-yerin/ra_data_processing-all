a677b39f3b4f161a26c90d6df80edc1e99a66e2c
YeS <yerin.serge@gmail.com>
2023-06-09 23:02:39 +0300

    style corrections

--- package_cas_a_secular_decrease/script_DAT_casA_secular_decrease_GURT_long.py
+++ package_cas_a_secular_decrease/script_DAT_casA_secular_decrease_GURT_long.py
@@ -7,13 +7,14 @@ import os
 #                              P A R A M E T E R S                              *
 # *******************************************************************************
 # Path to data files
-path_to_data = '../DATA/'  # '/media/data/PYTHON/ra_data_processing-all/'
+# path_to_data = '../DATA/'  # '/media/data/PYTHON/ra_data_processing-all/'
+path_to_data = 'e:/python/ra_data_processing-all/DATA/'  # '/media/data/PYTHON/ra_data_processing-all/'
 
 # Path to intermediate data files and results
 path_to_results = os.path.dirname(os.path.realpath(__file__)) + '/'  # 'DATA/'
 
 # Types of data to get
-typesOfData = ['CRe', 'CIm']
+types_of_data = ['CRe', 'CIm']
 
 # List of frequencies to build intensity changes vs. time and save to TXT file:
 freqList_GURT = [12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0,
@@ -22,34 +23,27 @@ freqList_GURT = [12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.
                  60.0, 61.0, 62.0, 63.0, 64.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 71.0, 72.0, 73.0, 74.0, 75.0]
 
 
-averOrMin = 0                    # Use average value (0) per data block or minimum value (1)
-StartStopSwitch = 1              # Read the whole file (0) or specified time limits (1)
-AutoStartStop = 1                # 1 - calculate depending on source in comment, 0 - use manual values
-AutoSourceSwitch = 1             # 1 - find sources in comment, 0 - use manually set source
-SpecFreqRange = 0                # Specify particular frequency range (1) or whole range (0)
-VminMan = -120                   # Manual lower limit of immediate spectrum figure color range
-VmaxMan = -10                    # Manual upper limit of immediate spectrum figure color range
-VminNormMan = 0                  # Manual lower limit of normalized dynamic spectrum figure color range (usually = 0)
-VmaxNormMan = 18                 # Manual upper limit of normalized dynamic spectrum figure color range (usually = 15)
-RFImeanConst = 6                 # Constant of RFI mitigation (usually = 8)
-customDPI = 300                  # Resolution of images of dynamic spectra
-colormap = 'jet'                 # Colormap of images of dynamic spectra ('jet' or 'Greys')
-ChannelSaveTXT = 1               # Save intensities at specified frequencies to TXT file
-ChannelSavePNG = 1               # Save intensities at specified frequencies to PNG file
-ListOrAllFreq = 0                # Take all frequencies of a list to save TXT and PNG? 1-All, 0-List
+aver_or_min = 0              # Use average value (0) per data block or minimum value (1)
+start_stop_switch = 1        # Read the whole file (0) or specified time limits (1)
+spec_freq_range = 0          # Specify particular frequency range (1) or whole range (0)
+VminMan = -120               # Manual lower limit of immediate spectrum figure color range
+VmaxMan = -10                # Manual upper limit of immediate spectrum figure color range
+VminNormMan = 0              # Manual lower limit of normalized dynamic spectrum figure color range (usually = 0)
+VmaxNormMan = 18             # Manual upper limit of normalized dynamic spectrum figure color range (usually = 15)
+rfi_mean_const = 6           # Constant of RFI mitigation (usually = 8)
+custom_dpi = 300             # Resolution of images of dynamic spectra
+colormap = 'jet'             # Colormap of images of dynamic spectra ('jet' or 'Greys')
+channel_save_txt = 1         # Save intensities at specified frequencies to TXT file
+channel_save_png = 1         # Save intensities at specified frequencies to PNG file
+list_or_all_freq = 0         # Take all frequencies of a list to save TXT and PNG? 1-All, 0-List
 AmplitudeReIm = 20 * 10**(-12)   # Color range of Re and Im dynamic spectra
                                  # 10 * 10**(-12) is typical value for CasA for interferometer of 2 GURT subarrays
 AmplitudeReIm_UTR2 = 20000 * 10**(-12)
 AmplitudeReIm_GURT = 20 * 10**(-12)
 
-
 # Begin and end frequency of dynamic spectrum (MHz)
 freq_start = 0.0
 freq_stop = 10.0
-
-# Begin and end time of dynamic spectrum ('yyyy-mm-dd hh:mm:ss')
-# dateTimeStart = '2019-07-19 00:00:00'
-# dateTimeStop =  '2019-07-23 04:00:00'
 
 # Begin and end frequency of TXT files to save (MHz)
 freq_start_txt = 8.0
@@ -63,7 +57,7 @@ import time
 import sys
 import os
 from os import path
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 from astropy.time import Time, TimeDelta
 
 # To change system path to main directory of the project:
@@ -76,6 +70,7 @@ from package_ra_data_files_formats.ADR_file_reader import ADR_file_reader
 from package_ra_data_files_formats.DAT_file_reader import DAT_file_reader
 from package_ra_data_files_formats.read_file_header_adr import file_header_adr_read
 from package_ra_data_files_formats.read_file_header_jds import file_header_jds_read
+from package_ra_data_files_formats.time_line_file_reader import time_line_file_reader
 from package_astronomy.culmination_time_utc_astroplan import culmination_time_utc_astroplan
 
 
@@ -86,11 +81,10 @@ print('\n\n\n\n\n\n\n\n   ******************************************************
 print('   *  ', Software_name, ' v.', Software_version, '  *  (c) YeS 2019')
 print('   ************************************************************************* \n\n\n')
 
-startTime = time.time()
+script_start_time = time.time()
 currentTime = time.strftime("%H:%M:%S")
 currentDate = time.strftime("%d.%m.%Y")
 print('   Today is ', currentDate, ' time is ', currentTime, '\n')
-
 
 # Search needed files in the directory and subdirectories
 file_name_list = find_files_only_in_current_folder(path_to_data, '.txt', 0)
@@ -110,7 +104,7 @@ for i in range(len(timeline_file_name_list)):
 # Loop by data types selected by user
 print('\n Data files found: \n')
 dat_files_list = []
-for type_of_data in typesOfData:
+for type_of_data in types_of_data:
     name = data_files_name_list[0] + '_Data_' + type_of_data + '.dat'
     print(' - ' + path_to_data + name)
     if os.path.isfile(path_to_data + name):
@@ -119,25 +113,7 @@ for type_of_data in typesOfData:
 
 # *** Reading timeline file ***
 name = path_to_data + timeline_file_name_list[0]
-TLfile = open(name, 'r')
-timeline = []
-for line in TLfile:
-    timeline.append(str(line))
-TLfile.close()
-
-# *** Converting text to ".datetime" format ***
-dt_timeline = []
-for i in range(len(timeline)):  # converting text to ".datetime" format
-
-    # Check is the uS field is empty. If so it means it is equal to '000000'
-    uSecond = timeline[i][20:26]
-    if len(uSecond) < 2:
-        uSecond = '000000'
-
-    dt_timeline.append(datetime(int(timeline[i][0:4]), int(timeline[i][5:7]), int(timeline[i][8:10]),
-                                int(timeline[i][11:13]), int(timeline[i][14:16]), int(timeline[i][17:19]),
-                                int(uSecond)))
-
+timeline, dt_timeline = time_line_file_reader(name)
 
 # *** Showing the time limits of file
 print('\n                                Start                         End \n')
@@ -163,6 +139,7 @@ for day in range(no_of_days+1):
     if (culm_time > Time(dt_timeline[0]) + TimeDelta(3600, format='sec')) and \
             (culm_time < Time(dt_timeline[-1]) - TimeDelta(3600, format='sec')):
         culm_time_3C405.append(culm_time)
+
     culm_time = Time(culmination_time_utc_astroplan('3C461', str(date), 0))
     if (culm_time > Time(dt_timeline[0]) + TimeDelta(3600, format='sec')) and \
             (culm_time < Time(dt_timeline[-1]) - TimeDelta(3600, format='sec')):
@@ -186,23 +163,28 @@ for i in range(len(culm_time_3C405)):
     start_time = culm_time_3C405[i] - TimeDelta(3600, format='sec')
     end_time = culm_time_3C405[i] + TimeDelta(3600, format='sec')
 
-    dateTimeStart = str(start_time)[0:19]
-    dateTimeStop = str(end_time)[0:19]
+    date_time_start = str(start_time)[0:19]
+    date_time_stop = str(end_time)[0:19]
 
     [df_filename, df_filesize, df_system_name, df_obs_place, df_description, CLCfrq, df_creation_timeUTC,
         ReceiverMode, Mode, sumDifMode, NAvr, TimeRes, fmin, fmax, df, frequency, FFTsize, SLine, Width, BlockSize] = \
         file_header_adr_read(path_to_data + dat_files_list[0], 0, 0)
 
     result_folder = data_files_name_list[0] + "_" + str(i+1) + '_of_' + str(len(culm_time_3C405)) + '_' + source
-    done_or_not = DAT_file_reader(path_to_data, data_files_name_list[0], typesOfData, result_folder, 'folder', averOrMin,
-                                  StartStopSwitch, SpecFreqRange, VminMan, VmaxMan, VminNormMan, VmaxNormMan,
-                                  RFImeanConst, customDPI, colormap, ChannelSaveTXT, ChannelSavePNG, ListOrAllFreq,
-                                  AmplitudeReIm_GURT, freq_start, freq_stop, dateTimeStart, dateTimeStop, freq_start_txt,
+    done_or_not = DAT_file_reader(path_to_data, data_files_name_list[0], types_of_data, result_folder, 'folder',
+                                  aver_or_min, start_stop_switch, spec_freq_range, VminMan, VmaxMan,
+                                  VminNormMan, VmaxNormMan, rfi_mean_const, custom_dpi, colormap,
+                                  channel_save_txt, channel_save_png, list_or_all_freq,
+                                  AmplitudeReIm_GURT, freq_start, freq_stop,
+                                  date_time_start, date_time_stop, freq_start_txt,
                                   freq_stop_txt, freqList_GURT, 0)
 
     # Saving TXT file with parameters from file header
     path = path_to_data + 'DAT_Results_' + result_folder + '/'
-    TXT_file = open(path + data_files_name_list[0]+'_'+source + '_header.info', "w")
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    TXT_file = open(path + data_files_name_list[0] + '_' + source + '_header.info', "w")
     TXT_file.write(' Observatory:           ' + df_obs_place + '\n')
     TXT_file.write(' Receiver:              ' + df_system_name + '\n')
     TXT_file.write(' Initial filename:      ' + df_filename + '\n')
@@ -216,7 +198,7 @@ for i in range(len(culm_time_3C405)):
     TXT_file.close()
 
 
-del start_time, end_time, dateTimeStart, dateTimeStop
+del start_time, end_time, date_time_start, date_time_stop
 
 # In a loop take a two-hour fragments of data and make text files with responses
 ################################################################################
@@ -233,18 +215,20 @@ for i in range(len(culm_time_3C461)):
     start_time = culm_time_3C461[i] - TimeDelta(3600, format='sec')
     end_time = culm_time_3C461[i] + TimeDelta(3600, format='sec')
 
-    dateTimeStart = str(start_time)[0:19]
-    dateTimeStop = str(end_time)[0:19]
+    date_time_start = str(start_time)[0:19]
+    date_time_stop = str(end_time)[0:19]
 
     [df_filename, df_filesize, df_system_name, df_obs_place, df_description, CLCfrq, df_creation_timeUTC,
         ReceiverMode, Mode, sumDifMode, NAvr, TimeRes, fmin, fmax, df, frequency, FFTsize, SLine, Width, BlockSize] = \
         file_header_adr_read(path_to_data + dat_files_list[0], 0, 0)
 
     result_folder = data_files_name_list[0] + "_" + str(i+1) + '_of_' + str(len(culm_time_3C461)) + '_' + source
-    done_or_not = DAT_file_reader(path_to_data, data_files_name_list[0], typesOfData, result_folder, 'folder_2', averOrMin,
-                                  StartStopSwitch, SpecFreqRange, VminMan, VmaxMan, VminNormMan, VmaxNormMan,
-                                  RFImeanConst, customDPI, colormap, ChannelSaveTXT, ChannelSavePNG, ListOrAllFreq,
-                                  AmplitudeReIm_GURT, freq_start, freq_stop, dateTimeStart, dateTimeStop, freq_start_txt,
+    done_or_not = DAT_file_reader(path_to_data, data_files_name_list[0], types_of_data, result_folder, 'folder_2', 
+                                  aver_or_min, start_stop_switch, spec_freq_range, VminMan, VmaxMan, 
+                                  VminNormMan, VmaxNormMan, rfi_mean_const, custom_dpi, colormap, 
+                                  channel_save_txt, channel_save_png, list_or_all_freq,
+                                  AmplitudeReIm_GURT, freq_start, freq_stop, 
+                                  date_time_start, date_time_stop, freq_start_txt,
                                   freq_stop_txt, freqList_GURT, 0)
 
     # Saving TXT file with parameters from file header
@@ -263,7 +247,8 @@ for i in range(len(culm_time_3C461)):
     TXT_file.close()
 
 
-endTime = time.time()
+script_end_time = time.time()
 print('\n\n\n  The program execution lasted for ',
-      round((endTime - startTime), 2), 'seconds (', round((endTime - startTime)/60, 2), 'min. ) \n')
+      round((script_end_time - script_start_time), 2), 'seconds (',
+      round((script_end_time - script_start_time)/60, 2), 'min. ) \n')
 print('\n           *** Program ', Software_name, ' has finished! *** \n\n\n')
