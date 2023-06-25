19a47e069ffd562ba2e148367b8fca14cc0a88fc
YeS <yerin.serge@gmail.com>
2023-06-25 23:31:06 +0300

    test of time added

--- package_ra_data_files_formats/njds_spectrum_reader_temporary.py
+++ package_ra_data_files_formats/njds_spectrum_reader_temporary.py
@@ -48,7 +48,7 @@ with open(fname, 'rb') as file:
     file.seek(1024)  # Jumping to 1024 byte from file beginning
 
     if 0 < mode < 3:           # Spectra modes
-        n_sp = 1
+        n_sp = 2
         if mode == 2:
             raw = np.fromfile(file, dtype='u4', count=(4 * n_sp * freq_points_num))
 
@@ -62,10 +62,31 @@ with open(fname, 'rb') as file:
             del raw
 
             # *** Single out timing from data ***
-            # counter_a2 = np.uint64(data_cha[:, -1])
-            # counter_b2 = np.uint64(data_chb[:, -1])
-            # counter_a1 = np.uint64(data_cha[:, -2])
-            # counter_b1 = np.uint64(data_chb[:, -2])
+            counter_a2 = np.uint64(data_cha[:, -1])
+            counter_b2 = np.uint64(data_chb[:, -1])
+            counter_a1 = np.uint64(data_cha[:, -2])
+            counter_b1 = np.uint64(data_chb[:, -2])
+
+            # tmp = np.uint64(int('00111111111111111111111111111111', 2))
+            # us_count = np.uint32(np.bitwise_and(counter_b2, tmp))    # number of us since record started
+            # sp_count = np.uint32(np.bitwise_and(counter_a2, tmp))    # number of specter since record started
+
+            us_count = counter_b2  # number of us since record started
+            sp_count = counter_a2  # number of specter since record started
+
+            tmp = np.uint64(int('00000111111111111111111111111111', 2))
+            phase_of_sec = np.uint32(np.bitwise_and(counter_a1, tmp))  # phase of second for the specter
+            tmp = np.uint64(int('00000000000000011111111111111111', 2))
+            sec_of_day = np.uint32(np.bitwise_and(counter_b1, tmp))  # second of the day for the specter
+
+            print('us count:', us_count[0], us_count[1], '  dif=', np.int64(int(us_count[1]) - int(us_count[0])))
+            print('spectra count:', sp_count[0], sp_count[1], '  dif=', np.int64(int(sp_count[1]) - int(sp_count[0])))
+
+            print('sec from record start:', us_count[0] / 1000000, '\n')
+
+            print('Sec of day:', sec_of_day[0], sec_of_day[1], '  dif=', sec_of_day[1] - sec_of_day[0])
+            print('Phase of sec:', phase_of_sec[0], phase_of_sec[1], '  dif=', phase_of_sec[1] - phase_of_sec[0])
+            print('Sec of day:', timedelta(0, int(sec_of_day[0]), int(1000000 * phase_of_sec[0] / clc_freq)))
 
             # *** Converting from FPGA to PC float format ***
             # if mode == 1 or mode == 2:
