1fd0c5bb424082033de18961d940953bea5f3fd8
serge-yerin <yerin.serge@gmail.com>
2023-07-06 14:10:22 +0300

    added spectrum

--- package_sandbox/gui_plot_qt_test.py
+++ package_sandbox/gui_plot_qt_test.py
@@ -1,10 +1,12 @@
 import os
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QMainWindow
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+import PyQt5
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
-import random
+import numpy as np
+import numpy.ma as ma
 
 from package_astronomy.catalogue_pulsar import catalogue_pulsar
 from package_common_modules.text_manipulations import read_one_value_txt_file
@@ -21,7 +23,69 @@ def read_and_prepare_data(common_path, filename):
     profile_data = read_one_value_txt_file(data_filename)
     print('\n  Number of samples in text file:  ', len(profile_data))
 
-    return profile_data
+    # Getting pulsar parameters from catalogue
+    pulsar_ra, pulsar_dec, source_dm, pulsar_period = catalogue_pulsar(pulsar_name)
+
+    # Data frequency resolution, Hz
+    frequency_resolution = 1 / (time_resolution * len(profile_data))
+
+    # Calculate pulsar harmonics frequency
+    pulsar_frequency = 1 / pulsar_period  # frequency of pulses, Hz
+
+    frequency_limit = (harmonics_to_show + 1.5) * pulsar_frequency
+
+    freq_points_per_harmonic = np.ceil(pulsar_frequency / frequency_resolution).astype(int)
+    # n_harmonics = int(np.floor(len(profile_data) / (2 * freq_points_per_harmonic)))
+    n_harmonics = harmonics_to_show + 1
+
+    pulsar_harmonics = pulsar_frequency * np.linspace(1, n_harmonics, num=n_harmonics)
+    pulsar_harmonics_points = np.ceil(pulsar_harmonics / frequency_resolution).astype(int)
+    max_interval = int(freq_points_per_harmonic / 4)
+
+    print('  Pulsar frequency: ', pulsar_frequency, ' Hz')
+    print('  Frequency resolution: ', frequency_resolution, ' s')
+    print('  Time resolution: ', time_resolution, ' s')
+    print('  Number of points per harmonic: ', freq_points_per_harmonic)
+    print('  Number harmonics to highlight: ', n_harmonics)
+    print('  Interval to search the harmonic: ', max_interval, ' points')
+
+    # Calculating the spectrum
+    profile_spectrum = np.power(np.real(np.fft.fft(profile_data[:])), 2)  # calculation of the spectrum
+    profile_spectrum = profile_spectrum[0:int(len(profile_spectrum) / 2)]  # delete second part of the spectrum
+
+    # Nulling of lowest harmonics if necessary
+    # profile_spectrum[0:100] = 0.0
+
+    frequency_axis = [frequency_resolution * i for i in range(len(profile_spectrum))]
+
+    # Finding the maximal spectrum amplitudes near expected harmonics
+    max_harmonics = []
+    noise_mean = []
+    noise_std = []
+    signal_to_noise = []
+    for i in range(n_harmonics):
+        # Maximal points near expected harmonics
+        data_bunch_near_harmonic = profile_spectrum[pulsar_harmonics_points[i] - max_interval:
+                                                    pulsar_harmonics_points[i] + max_interval].copy()
+
+        max_near_harmonic = np.max(data_bunch_near_harmonic)
+        max_harmonics.append(max_near_harmonic)
+
+        # Masking the maximal value
+        data_bunch_near_harmonic_masked = ma.masked_values(data_bunch_near_harmonic, max_near_harmonic, copy=True)
+
+        # Calculating mean and std of noise around harmonic but without it
+        current_noise_mean = np.mean(data_bunch_near_harmonic_masked)
+        current_noise_std = np.std(data_bunch_near_harmonic_masked)
+
+        noise_mean.append(current_noise_mean)
+        noise_std.append(3 * current_noise_std)
+        signal_to_noise.append(max_near_harmonic / (current_noise_mean + 3 * current_noise_std))
+
+    # Calculating the limit o vertical axis of the spectrum plot
+    spectrum_max = np.max(max_harmonics)
+
+    return profile_data,  frequency_axis, profile_spectrum, spectrum_max, frequency_limit
 
 
 # main window which inherits QDialog
@@ -31,7 +95,10 @@ class Window(QDialog):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
 
-        self.setWindowTitle('Application')  # Window title
+        # self.setWindowFlag(PyQt5.WindowMinimizeButtonHint, True)
+        # self.setWindowFlag(PyQt5.WindowMaximizeButtonHint, True)
+
+        self.setWindowTitle('Pulsar profiles analysis')  # Window title
         self.figure = plt.figure()  # a figure instance to plot on
 
         # this is the Canvas Widget that displays the 'figure' it takes the 'figure' instance as a parameter to __init__
@@ -68,28 +135,34 @@ class Window(QDialog):
     # action called by the push button
     def plot(self):
         # random data
-        data_0 = read_and_prepare_data(common_path, filename)
-        data_1 = [random.random() for i in range(100)]
+        data_0, x_line_1, data_1, spectrum_max, frequency_limit = read_and_prepare_data(common_path, filename)
 
         self.figure.clear()  # clearing old figure
         ax0 = self.figure.add_subplot(211)
         ax0.plot(data_0, 'o-')
+        ax0.set_xlim([0, len(data_0)])
         ax0.set_ylim([-0.2, 0.2])
-        ax0.set_title('Title')
+        ax0.set_title('Time series')
         ax1 = self.figure.add_subplot(212)
-        ax1.plot(data_1, 'o-')
-        ax1.set_ylim([0, 1])
-        ax1.set_title('Title')
+        # Adding the plots for parts of data to the big result picture
+        ax1.plot(x_line_1, data_1)
+        ax1.axis([0, frequency_limit, 0, 1.1 * spectrum_max])
+        self.figure.subplots_adjust(hspace=0.25, top=0.945)
+        ax1.set_title('Spectrum')
         self.canvas.draw()  # refresh canvas
 
 
-# driver code
+# Driver code
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # creating apyqt5 application
     main = Window()  # creating a window object
     main.show()  # showing the window
 
+    # Parameters
     common_path = '../../../RA_DATA_ARCHIVE/ADDITIONAL_pulses_profiles/'
     filename = 'B0329+54_DM_26.78_C240122_152201.jds_Data_chA_time_profile.txt'
+    pulsar_name = 'B0329+54'
+    harmonics_to_show = 15  # Figure upper frequency (x-axis) limit in number of pulse harmonics to show
+    time_resolution = (1 / 66000000) * 16384 * 32  # Data time resolution, s   # 0.007944
 
     sys.exit(app.exec_())  # loop
