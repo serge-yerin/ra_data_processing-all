e1c6755fc846c7018dc786784993bc5df69726b9
serge-yerin <yerin.serge@gmail.com>
2023-07-06 11:13:38 +0300

    added reading data

--- package_sandbox/gui_plot_qt_test.py
+++ package_sandbox/gui_plot_qt_test.py
@@ -1,9 +1,27 @@
+import os
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QMainWindow
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 import random
+
+from package_astronomy.catalogue_pulsar import catalogue_pulsar
+from package_common_modules.text_manipulations import read_one_value_txt_file
+
+
+def read_and_prepare_data(common_path, filename):
+    data_filename = common_path + filename
+
+    new_folder_name = filename[:-4] + "_analysis"
+    if not os.path.exists(common_path + new_folder_name):
+        os.makedirs(common_path + new_folder_name)
+
+    # Reading profile data from txt file
+    profile_data = read_one_value_txt_file(data_filename)
+    print('\n  Number of samples in text file:  ', len(profile_data))
+
+    return profile_data
 
 
 # main window which inherits QDialog
@@ -13,10 +31,8 @@ class Window(QDialog):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
 
-        self.setWindowTitle('Application')
-
-        # a figure instance to plot on
-        self.figure = plt.figure()
+        self.setWindowTitle('Application')  # Window title
+        self.figure = plt.figure()  # a figure instance to plot on
 
         # this is the Canvas Widget that displays the 'figure' it takes the 'figure' instance as a parameter to __init__
         self.canvas = FigureCanvas(self.figure)
@@ -30,7 +46,7 @@ class Window(QDialog):
         # adding action to the button
         self.button.clicked.connect(self.plot)
 
-        # Just some button connected to 'plot' method
+        # Just some button connected to 'Calc' method
         self.button2 = QPushButton('Calc')
 
         # creating a Vertical Box layout
@@ -50,38 +66,30 @@ class Window(QDialog):
         self.setLayout(layout)
 
     # action called by the push button
-    def plot(self, *args, **kwargs):
+    def plot(self):
         # random data
-        data_0 = [random.random() for i in range(100)]
+        data_0 = read_and_prepare_data(common_path, filename)
         data_1 = [random.random() for i in range(100)]
 
-        # clearing old figure
-        self.figure.clear()
-
+        self.figure.clear()  # clearing old figure
         ax0 = self.figure.add_subplot(211)
         ax0.plot(data_0, 'o-')
-        ax0.set_ylim([0, 1])
+        ax0.set_ylim([-0.2, 0.2])
         ax0.set_title('Title')
-
         ax1 = self.figure.add_subplot(212)
         ax1.plot(data_1, 'o-')
         ax1.set_ylim([0, 1])
         ax1.set_title('Title')
-
-        # refresh canvas
-        self.canvas.draw()
+        self.canvas.draw()  # refresh canvas
 
 
 # driver code
 if __name__ == '__main__':
-    # creating apyqt5 application
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv)  # creating apyqt5 application
+    main = Window()  # creating a window object
+    main.show()  # showing the window
 
-    # creating a window object
-    main = Window()
+    common_path = '../../../RA_DATA_ARCHIVE/ADDITIONAL_pulses_profiles/'
+    filename = 'B0329+54_DM_26.78_C240122_152201.jds_Data_chA_time_profile.txt'
 
-    # showing the window
-    main.show()
-
-    # loop
-    sys.exit(app.exec_())
+    sys.exit(app.exec_())  # loop
