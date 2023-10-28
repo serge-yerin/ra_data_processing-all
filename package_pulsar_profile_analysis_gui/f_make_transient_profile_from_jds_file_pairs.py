e5f23af87d39b7f35de7309e8bb5c4f024120a6f
Serge Yerin <yerin.serge@gmail.com>
2023-10-28 22:18:38 +0300

    pairs processing is ready, updated comments

--- package_pulsar_profile_analysis_gui/f_make_transient_profile_from_jds_file_pairs.py
+++ package_pulsar_profile_analysis_gui/f_make_transient_profile_from_jds_file_pairs.py
@@ -1,5 +1,4 @@
 import sys
-import numpy as np
 from os import path
 
 # To change system path to main directory of the project:
@@ -12,12 +11,20 @@ from package_pulsar_profile_analysis_gui.f_make_transient_profile_from_jds impor
 
 def f_make_transient_profile_from_jds_file_pairs(source_directory, result_directory, source_dm):
 
+    # Checking if the paths end with slash
+    if source_directory[-1] != '/':
+        source_directory = source_directory + '/'
+    if result_directory[-1] != '/':
+        result_directory = result_directory + '/'
+
+    # Finding a list of jds files in the folder (output is sorted by name)
     file_list = find_files_only_in_current_folder(source_directory, '.jds', True)
 
+    # Creating pairs and run the processing pipeline part
     for pair in range(len(file_list) - 1):
-        file_name_list_current = [file_list[pair], file_list[pair + 1]]
 
-        print('  Pair of files #', pair + 1)
+        file_name_list_current = [file_list[pair], file_list[pair + 1]]
+        print('\n\n Pair of files #', pair + 1)
 
         make_transient_profile_from_jds(source_directory, file_name_list_current, result_directory, source_dm)
 
