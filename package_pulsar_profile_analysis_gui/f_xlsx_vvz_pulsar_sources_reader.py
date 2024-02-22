8784ecee84bf70c4d035c51405822f6268dc6532
Serge Yerin <yerin.serge@gmail.com>
2024-02-22 23:19:12 +0200

    added filtering by values and saving to arrays

--- package_pulsar_profile_analysis_gui/f_xlsx_vvz_pulsar_sources_reader.py
+++ package_pulsar_profile_analysis_gui/f_xlsx_vvz_pulsar_sources_reader.py
@@ -24,11 +24,32 @@ def xlsx_vvz_pulsar_sources_reader(data_path, excel_file_name, excel_sheet_name)
 
     print('\n\n')
 
+    # Filling the merged cells by the value in the first row of such cells
     dataframe['Code&Date'] = pd.Series(dataframe['Code&Date']).fillna(method='ffill')
     dataframe['Beam'] = pd.Series(dataframe['Beam']).fillna(method='ffill')
 
     print(dataframe.head(40))
 
+    print('\n\n')
+
+    # Selecting lines (rows) fronm correct code&date with indicated paths to files
+    selected_df = dataframe[dataframe['Code&Date'].str.contains('V102')]
+    selected_df = selected_df[selected_df['Path 1'].notna()]
+    print (selected_df)
+
+    code_date = []
+    beam = []
+    source_dm = []
+    file_paths = []
+
+    for i in range (selected_df.shape[0]):
+        code_date.append(selected_df.iloc[i]['Code&Date'])
+        beam.append(selected_df.iloc[i]['Beam'])
+        source_dm.append(selected_df.iloc[i]['DM_corr'])
+        file_paths.append([selected_df.iloc[i]['Path 1'], selected_df.iloc[i]['Path 2']])
+
+    for i in range (selected_df.shape[0]):
+        print(code_date[i], beam[i], source_dm[i], file_paths[i])
 
     return
 
