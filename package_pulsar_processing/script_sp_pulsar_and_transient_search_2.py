25110838eb7c42ce90e9cea25d29a1e211565d43
Serge Yerin <yerin.serge@gmail.com>
2024-07-20 19:15:58 +0300

    first spectra tryouts

--- package_pulsar_processing/script_sp_pulsar_and_transient_search_2.py
+++ package_pulsar_processing/script_sp_pulsar_and_transient_search_2.py
@@ -9,10 +9,10 @@ software_name = 'Transient Search Script 2'
 # *******************************************************************************
 # Directory of files to be analyzed:
 # source_directory = '../../RA_DATA_ARCHIVE/DSP_spectra_pulsar_UTR2_B0950+08/'
-source_directory = '../RA_DATA_RESULTS/Transient_search_DSP_cross_spectra_B0809+74_URAN2/'
+source_directory = '../RA_DATA_RESULTS/Transient_search_DSP_cross_spectra_B0809+74_URAN2_/'
 vardmd_file_name = 'Transient_P130422_121607.jds_Data_chA_var_DM_5.255-6.255.vdm'
 timeln_file_name = 'Transient_P130422_121607.jds_Data_chA_var_DM_5.255-6.255_Timeline.txt'
-result_directory = '../RA_DATA_RESULTS/Transient_search_DSP_cross_spectra_B0809+74_URAN2/'
+result_directory = '../RA_DATA_RESULTS/Transient_search_DSP_cross_spectra_B0809+74_URAN2_/'
 
 # central_dm = 2.972
 central_dm = 5.755
@@ -79,5 +79,55 @@ vdm_data, dm_vector = read_and_plot_var_dm_file(source_directory, vardmd_file_na
 
 
 print(vdm_data.shape)
+
+
+frequency_resolution = 1 / (time_res * vdm_data.shape[1]) # frequency resolution, Hz
+
+
+profile_spectrum = np.power(np.real(np.fft.fft(vdm_data[:])), 2)  # calculation of the spectrum
+profile_spectrum = profile_spectrum[:, 0 : int(vdm_data.shape[1]/2)]  # delete second part of the spectrum
+profile_spectrum = np.log10(profile_spectrum)
+
+#profile_spectrum[0:100] = 0.0
+
+frequency_axis = [frequency_resolution * i for i in range(len(profile_spectrum))]
+
+
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+# Making result picture
+fig = plt.figure(figsize=(9.2, 4.5))
+# rc('font', size=5, weight='bold')
+ax1 = fig.add_subplot(111)
+# ax1.plot(profile_spectrum[0, :], color=u'#1f77b4', linestyle='-', alpha=1.0, linewidth='0.60', label='Pulses time profile')
+# ax1.legend(loc='upper right', fontsize=5)
+# ax1.grid(b=True, which='both', color='silver', linewidth='0.50', linestyle='-')
+# ax1.axis([0, len(profile_data), profile_pic_min, profile_pic_max])
+# ax1.set_ylabel('Amplitude, AU', fontsize=6, fontweight='bold')
+# ax1.set_title('File: ' + filename + '  Pulsar period: '+str(np.round(pulsar_period,3))+' s.', fontsize=5, fontweight='bold')
+# ax1.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+# ax2 = fig.add_subplot(212)
+
+ax1.imshow(profile_spectrum[0:100, 0:500], aspect='auto')
+ax1.set_xticklabels(frequency_axis)
+ax1.set_yticklabels(dm_vector)
+
+# ax1.axis([frequency_axis[0], frequency_axis[-1], -10, 10])
+# ax2.legend(loc='upper right', fontsize=5)
+# ax2.set_xlabel('Frequency, Hz', fontsize=6, fontweight='bold')
+# ax2.set_ylabel('Amplitude, AU', fontsize=6, fontweight='bold')
+# fig.subplots_adjust(hspace=0.05, top=0.91)
+# fig.suptitle('Single pulses of ' + pulsar_name + ' in time and frequency', fontsize=7, fontweight='bold')
+# fig.text(0.80, 0.04, 'Processed ' + currentDate + ' at ' + currentTime, fontsize=3, transform=plt.gcf().transFigure)
+# fig.text(0.09, 0.04, 'Software version: ' + Software_version + ', yerin.serge@gmail.com, IRA NASU', fontsize=3,
+#          transform=plt.gcf().transFigure)
+# pylab.savefig(common_path + '/' + filename[0:-4] + ' and its spectrum.png', bbox_inches='tight', dpi=customDPI)
+plt.show()
+plt.close('all')
+
+
+
+
 
 print('\n\n  * ', str(datetime.datetime.now())[:19], ' * Pipeline finished successfully! \n\n')
