4bc5d618fc8a00e848eb957b9f26bd72851729df
Serge Yerin <yerin.serge@gmail.com>
2024-07-24 20:10:56 +0300

    updated with true multiprocessing

--- package_sandbox/multiprocessing_example.py
+++ package_sandbox/multiprocessing_example.py
@@ -1,3 +1,4 @@
+"""
 from multiprocessing import Process
 import time
 
@@ -23,3 +24,39 @@ if __name__ == '__main__':
     p1 = Process(target=f1, args=())
     p1.start()
     p1.join()
+
+"""
+
+import multiprocessing 
+
+def square(x, *args, **kwargs) : # , **kwargs
+    
+    print("\n",  x, args[0], kwargs, "\n")
+    
+    return kwargs["n"] 
+  
+if __name__ == '__main__': 
+    n_proc = 3
+    pool = multiprocessing.Pool() 
+    my_kwargs_1 = {'n': 3}
+    my_kwargs_2 = {'a': 5}
+    args = ("a", )
+    
+    result_async = [pool.apply_async(square, (1, ) + args , {**{"c": i}, **my_kwargs_1}) for i in range(10)] 
+    
+    results = [r.get() for r in result_async] 
+
+    print("Output: {}".format(results)) 
+    
+    
+    
+# import multiprocessing 
+
+# def square(x): 
+#     return x * x 
+
+# if __name__ == '__main__': 
+#     pool = multiprocessing.Pool() 
+#     result_async = [pool.apply_async(square, args = (i, )) for i in range(10)] 
+#     results = [r.get() for r in result_async]
+#     print("Output: {}".format(results))
