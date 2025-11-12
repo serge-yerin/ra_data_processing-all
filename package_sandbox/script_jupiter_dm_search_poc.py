c045dc15e0f758beb4038963b9d2e9a8dca7b74f
serge-yerin <yerin.serge@gmail.com>
2025-11-13 00:16:25 +0200

    multiband

--- package_sandbox/script_jupiter_dm_search_poc.py
+++ package_sandbox/script_jupiter_dm_search_poc.py
@@ -19,19 +19,39 @@ from package_ra_data_files_formats.time_line_file_reader import time_line_file_r
 from package_ra_data_processing.filtering import median_filter, average_filter
 
 initial_dat_file_path = "../ra_data_processing-all/P130725_075910.jds_Data_chA.dat"
+# initial_dat_file_path = "../ra_data_processing-all/P130725_082052.jds_Data_chA.dat"
+# initial_dat_file_path = "../ra_data_processing-all/P130725_084023.jds_Data_chA.dat"
 print_or_not = True
 spec_freq_range = True
 
-window_start_index = 3500
-window_stop_index = 6500
-target_freq_index = 800
+window_start_index = [3000, 4000, 5000]  #3500 
+window_stop_index = [4000, 5000, 6000]  #6500
+target_freq_index = 1  #800
 block_length_in_spectra = 100
-n = 100 # number of minima to be used for RM calculation    
+num_of_points = [120, 60, 70] # number of minima to be used for RM calculation 100    
 
 speed_of_light = 2.99792458e8 # meters per second
 
-print('\n\n\nLowest frequency to be analyzed:', window_start_index * (66.0 / 16384), 'MHz')
-print('\nHighest frequency to be analyzed:', window_stop_index * (66.0 / 16384), 'MHz\n\n\n')
+
+
+def find_closest_value(data_list, target_value):
+    """
+    Finds the value in a list that is closest to a given target value.
+
+    Args:
+        data_list (list): The list of numbers to search within.
+        target_value (int or float): The value to find the closest element to.
+
+    Returns:
+        int or float: The value from the list that is closest to the target_value.
+    """
+    if not data_list:
+        raise ValueError("The input list cannot be empty.")
+
+    closest_value = min(data_list, key=lambda x: abs(x - target_value))
+    return closest_value
+
+
 
 # Path to timeline file to be analyzed:
 initial_time_line_file_name = initial_dat_file_path[-31:-13] + '_Timeline.txt'
@@ -80,206 +100,213 @@ for i in range(len(reduced_timeline)):
 
 
 dat_file = open(initial_dat_file_path, 'rb')
-dat_file.seek(1024)    # Jumping to 1024 byte from file beginning
-
-jupiter_rm_vs_time = []
-jupiter_rm_std_vs_time = []
-
-for block in range(num_of_blocks):   # main loop by number of blocks in file
-
-    if print_or_not:
-        print('\n * Data block # ', block + 1, ' of ', num_of_blocks,
-                '\n ******************************************************************')
-
-    # Timeline arrangements:
-    fig_time_scale = []
-    fig_date_time_scale = []
-    for i in range(block * block_length_in_spectra, (block+1) * block_length_in_spectra):  
-        fig_time_scale.append(timeline[i][11:23])
-        fig_date_time_scale.append(timeline[i][:])
-    if print_or_not:
-        print(' Time: ', fig_time_scale[0], ' - ', fig_time_scale[-1], ', number of points: ', len(fig_time_scale))
-
-    # Data block reading
-    if receiver_type == '.jds':
-        data = np.fromfile(dat_file, dtype=np.float64, count=(num_frequencies) * block_length_in_spectra)   # 2
-        data = np.reshape(data, [(num_frequencies), block_length_in_spectra], order='F')  # 2
-        data = data[:num_frequencies-4, :]  # To delete the last channels of DSP data where time is stored
+# dat_file.seek(1024)    # Jumping to 1024 byte from file beginning
 
 
-    data_init = data.copy()
+jupiter_rm_vs_time_vs_freq = np.empty((3, num_of_blocks))
+freq_bands_str = []
 
-    # plt.imshow(10 * np.log10(data), aspect='auto', origin='lower', cmap='jet')
-    # plt.show()
-
-    # Preparing single averaged data profile for figure
-    profile = data_init.mean(axis=1)[:]
-
-    # plt.plot(10 * np.log10(profile))
-    # plt.show()
-
-    lf_profile = median_filter(profile, 120)
-
-    # plt.plot(10 * np.log10(lf_profile))
-    # plt.show()
-
-    profile = profile / lf_profile
-
-    # plt.plot(10 * np.log10(profile))
-    # plt.show()
-
-    # window_start_index = 3500
-    # window_stop_index = 6500
-    profile = profile[window_start_index: window_stop_index]
-
-    # plt.plot(10 * np.log10(profile))
-    # plt.show()
-
-    from scipy.ndimage import uniform_filter1d
-    mean_profile = uniform_filter1d(profile, size=20)
-
-    from scipy.signal import argrelmin
-    indices = argrelmin(profile)[0]
-    # print(indices)
-
-    # plt.plot(10 * np.log10(profile))
-    # plt.plot(10 * np.log10(mean_profile))
-    # plt.plot(indices, 10 * np.log10(profile[indices]), 'ro')
-    # plt.show()
-
-    filtered_indices = []
-    for i in range(len(indices)-1):
-
-        if profile[indices[i]] < mean_profile[indices[i]]:
-            filtered_indices.append(indices[i]) 
-
-    # plt.plot(10 * np.log10(profile))
-    # plt.plot(10 * np.log10(mean_profile))
-    # plt.plot(filtered_indices, 10 * np.log10(profile[filtered_indices]), 'ro')
-    # plt.show()
-
-    # plt.plot(indices, 'bo')
-    # plt.plot(filtered_indices, 'ro')
-    # plt.show()
-
-    only_minima_profile = profile[filtered_indices]
-
-    # fig, axs = plt.subplots(2, 1) # Create a figure and a 1x2 grid of axes
-    # axs[0].plot(10 * np.log10(profile))
-    # axs[0].plot(10 * np.log10(mean_profile))
-    # # axs[0].plot(filtered_indices, 10 * np.log10(profile[filtered_indices]), 'ro')
-    # axs[0].plot(filtered_indices, 10 * np.log10(only_minima_profile), 'ro')
-    # axs[0].set_title("Plot 1")
-    # axs[1].plot(indices, 'bo')
-    # axs[1].plot(filtered_indices, 'ro')
-    # axs[1].set_title("Plot 2")
-    # plt.tight_layout()
-    # plt.show()
-    # plt.close('all')    
-
-    # print(filtered_indices)
-
-    def find_closest_value(data_list, target_value):
-        """
-        Finds the value in a list that is closest to a given target value.
-
-        Args:
-            data_list (list): The list of numbers to search within.
-            target_value (int or float): The value to find the closest element to.
-
-        Returns:
-            int or float: The value from the list that is closest to the target_value.
-        """
-        if not data_list:
-            raise ValueError("The input list cannot be empty.")
-
-        closest_value = min(data_list, key=lambda x: abs(x - target_value))
-        return closest_value
+for band in range(3):  
 
 
-    # target_freq_index = 800
-    closest = find_closest_value(filtered_indices, target_freq_index)
-    index_of_closest = filtered_indices.index(closest)
-    print("\n Index of closest value:", index_of_closest)
+    jupiter_rm_vs_time = []
+    jupiter_rm_std_vs_time = []
+
+    dat_file.seek(1024)    # Jumping to 1024 byte from file beginning
+
+    f_low = str(np.round(window_start_index[band] * (66.0 / 16384), 3))  # MHz   
+    f_high = str(np.round(window_stop_index[band] * (66.0 / 16384), 3))  # MHz
+
+    print('\n\n\nLowest frequency to be analyzed:', f_low, 'MHz')
+    print('\nHighest frequency to be analyzed:', f_high, 'MHz\n\n\n')
+
+    freq_bands_str.append(f_low + ' - ' + f_high + ' MHz')  
+
+    for block in range(num_of_blocks):   # main loop by number of blocks in file
+
+        if print_or_not:
+            print('\n * Data block # ', block + 1, ' of ', num_of_blocks,
+                    '\n ******************************************************************')
+
+        # Timeline arrangements:
+        fig_time_scale = []
+        fig_date_time_scale = []
+        for i in range(block * block_length_in_spectra, (block+1) * block_length_in_spectra):  
+            fig_time_scale.append(timeline[i][11:23])
+            fig_date_time_scale.append(timeline[i][:])
+        if print_or_not:
+            print(' Time: ', fig_time_scale[0], ' - ', fig_time_scale[-1], ', number of points: ', len(fig_time_scale))
+
+        # Data block reading
+        if receiver_type == '.jds':
+            data = np.fromfile(dat_file, dtype=np.float64, count=(num_frequencies) * block_length_in_spectra)   # 2
+            data = np.reshape(data, [(num_frequencies), block_length_in_spectra], order='F')  # 2
+            data = data[:num_frequencies-4, :]  # To delete the last channels of DSP data where time is stored
 
 
-    f_target = (66000000.0 / 16384) * (window_start_index + target_freq_index)
+        data_init = data.copy()
 
-    print("\n Target Frequency:", f_target, "Hz")
+        # plt.imshow(10 * np.log10(data), aspect='auto', origin='lower', cmap='jet')
+        # plt.show()
+
+        # Preparing single averaged data profile for figure
+        profile = data_init.mean(axis=1)[:]
+
+        # plt.plot(10 * np.log10(profile))
+        # plt.show()
+
+        lf_profile = median_filter(profile, 120)
+
+        # plt.plot(10 * np.log10(lf_profile))
+        # plt.show()
+
+        profile = profile / lf_profile
+
+        # plt.plot(10 * np.log10(profile))
+        # plt.show()
+
+        # window_start_index = 3500
+        # window_stop_index = 6500
+        profile = profile[window_start_index[band]: window_stop_index[band]]
+
+        # plt.plot(10 * np.log10(profile))
+        # plt.show()
+
+        from scipy.ndimage import uniform_filter1d
+        mean_profile = uniform_filter1d(profile, size=20)
+
+        from scipy.signal import argrelmin
+        indices = argrelmin(profile)[0]
+        # print(indices)
+
+        # plt.plot(10 * np.log10(profile))
+        # plt.plot(10 * np.log10(mean_profile))
+        # plt.plot(indices, 10 * np.log10(profile[indices]), 'ro')
+        # plt.show()
+
+        filtered_indices = []
+        for i in range(len(indices)-1):
+
+            if profile[indices[i]] < mean_profile[indices[i]]:
+                filtered_indices.append(indices[i]) 
+
+        if block == 0:
+            plt.plot(10 * np.log10(profile))
+            plt.plot(10 * np.log10(mean_profile))
+            plt.plot(filtered_indices, 10 * np.log10(profile[filtered_indices]), 'ro')
+            plt.show()
+
+        # plt.plot(indices, 'bo')
+        # plt.plot(filtered_indices, 'ro')
+        # plt.show()
+
+        only_minima_profile = profile[filtered_indices]
+
+        # fig, axs = plt.subplots(2, 1) # Create a figure and a 1x2 grid of axes
+        # axs[0].plot(10 * np.log10(profile))
+        # axs[0].plot(10 * np.log10(mean_profile))
+        # # axs[0].plot(filtered_indices, 10 * np.log10(profile[filtered_indices]), 'ro')
+        # axs[0].plot(filtered_indices, 10 * np.log10(only_minima_profile), 'ro')
+        # axs[0].set_title("Plot 1")
+        # axs[1].plot(indices, 'bo')
+        # axs[1].plot(filtered_indices, 'ro')
+        # axs[1].set_title("Plot 2")
+        # plt.tight_layout()
+        # plt.show()
+        # plt.close('all')    
+
+        # print(filtered_indices)
 
 
-    if closest < target_freq_index:
-        index_of_closest += 1
-        closest = filtered_indices[index_of_closest]
-
-    print("Closest frequency index above target:", closest)
-
-    # n = 100
-    list_of_closest_indices = list(range(0, n))
-    for i in range(len(list_of_closest_indices)):
-        list_of_closest_indices[i] += index_of_closest
-    # print("List of closest frequency indices above target:", list_of_closest_indices)
-
-    list_of_closest_points = []
-    for i in range(len(list_of_closest_indices)):
-        list_of_closest_points.append( filtered_indices[list_of_closest_indices[i]] )
-    # print("\nList of closest frequency points above target:", list_of_closest_points, "\n")
-
-    list_minima_frequencies = []
-    for i in range(len(list_of_closest_indices)):
-        list_minima_frequencies.append( (66000000.0 / 16384) * (window_start_index + list_of_closest_points[i]) )
-    # print("\nList of minima frequencies above target:", list_minima_frequencies, "\n")
-
-    list_delta_frequencies = []
-    for i in range(len(list_minima_frequencies)-1):
-        list_delta_frequencies.append( list_minima_frequencies[i+1] - list_minima_frequencies[i] )
-    # print("\nList of delta frequencies above target:", list_delta_frequencies, "\n")
-
-    list_central_frequencies = []
-    for i in range(len(list_minima_frequencies)-1):
-        list_central_frequencies.append( (list_minima_frequencies[i+1] + list_minima_frequencies[i]) / 2 )
-    # print("\nList of central frequencies above target:", list_central_frequencies, "\n")
+        # target_freq_index = 800
+        closest = find_closest_value(filtered_indices, target_freq_index)
+        index_of_closest = filtered_indices.index(closest)
+        print("\n Index of closest value:", index_of_closest)
 
 
-    list_rm_values = []
-    main_const = np.pi / speed_of_light**2
-    for i in range(len(list_delta_frequencies)):
+        f_target = (66000000.0 / 16384) * (window_start_index[band] + target_freq_index)
+
+        print("\n Target Frequency:", f_target, "Hz")
+
+
+        if closest < target_freq_index:
+            index_of_closest += 1
+            closest = filtered_indices[index_of_closest]
+
+        print("Closest frequency index above target:", closest)
+
+        # n = 100
+        list_of_closest_indices = list(range(0, num_of_points[band]))  # 100
+        for i in range(len(list_of_closest_indices)):
+            list_of_closest_indices[i] += index_of_closest
+        # print("List of closest frequency indices above target:", list_of_closest_indices)
+
+        list_of_closest_points = []
+        for i in range(len(list_of_closest_indices)):
+            list_of_closest_points.append( filtered_indices[list_of_closest_indices[i]] )
+        # print("\nList of closest frequency points above target:", list_of_closest_points, "\n")
+
+        list_minima_frequencies = []
+        for i in range(len(list_of_closest_indices)):
+            list_minima_frequencies.append( (66000000.0 / 16384) * (window_start_index[band] + list_of_closest_points[i]) )
+        # print("\nList of minima frequencies above target:", list_minima_frequencies, "\n")
+
+        list_delta_frequencies = []
+        for i in range(len(list_minima_frequencies)-1):
+            list_delta_frequencies.append( list_minima_frequencies[i+1] - list_minima_frequencies[i] )
+        # print("\nList of delta frequencies above target:", list_delta_frequencies, "\n")
+
+        list_central_frequencies = []
+        for i in range(len(list_minima_frequencies)-1):
+            list_central_frequencies.append( (list_minima_frequencies[i+1] + list_minima_frequencies[i]) / 2 )
+        # print("\nList of central frequencies above target:", list_central_frequencies, "\n")
+
+
+        list_rm_values = []
+        main_const = np.pi / speed_of_light**2
+        for i in range(len(list_delta_frequencies)):
+            
+            # list_rm_values.append( (np.pi / speed_of_light**2) * list_central_frequencies[i]**3 / (2 * list_delta_frequencies[i]))
+            
+            numerator = list_central_frequencies[i]**2 * (list_central_frequencies[i] + list_delta_frequencies[i])**2
+            denominator = (list_central_frequencies[i] + list_delta_frequencies[i])**2 - list_central_frequencies[i]**2
+            
+            list_rm_values.append(main_const * numerator / denominator)
+            
+        # print("\nList of RM values:", list_rm_values, "\n")
+
+        time_point_median_rm = np.median(list_rm_values)
+        time_point_std_rm = np.std(list_rm_values)
+        print(time_point_median_rm, time_point_std_rm)
         
-        # list_rm_values.append( (np.pi / speed_of_light**2) * list_central_frequencies[i]**3 / (2 * list_delta_frequencies[i]))
+        list_rm_values = np.array(list_rm_values)
         
-        numerator = list_central_frequencies[i]**2 * (list_central_frequencies[i] + list_delta_frequencies[i])**2
-        denominator = (list_central_frequencies[i] + list_delta_frequencies[i])**2 - list_central_frequencies[i]**2
+        import numpy.ma as ma
+        mask = np.abs(list_rm_values - time_point_median_rm) > 2 * time_point_std_rm
+        masked_data = ma.masked_array(list_rm_values, mask=mask)
+        time_point_std_rm = np.std(masked_data)
         
-        list_rm_values.append(main_const * numerator / denominator)
+        print(time_point_median_rm, time_point_std_rm)
         
-    # print("\nList of RM values:", list_rm_values, "\n")
-
-    time_point_median_rm = np.median(list_rm_values)
-    time_point_std_rm = np.std(list_rm_values)
-    print(time_point_median_rm, time_point_std_rm)
+        jupiter_rm_vs_time.append(time_point_median_rm)
+        jupiter_rm_std_vs_time.append(time_point_std_rm)
     
-    list_rm_values = np.array(list_rm_values)
-    
-    import numpy.ma as ma
-    mask = np.abs(list_rm_values - time_point_median_rm) > 2 * time_point_std_rm
-    masked_data = ma.masked_array(list_rm_values, mask=mask)
-    time_point_std_rm = np.std(masked_data)
-    
-    print(time_point_median_rm, time_point_std_rm)
-    
-    jupiter_rm_vs_time.append(time_point_median_rm)
-    jupiter_rm_std_vs_time.append(time_point_std_rm)
-    
+    jupiter_rm_vs_time_vs_freq[band, :] = jupiter_rm_vs_time
 
 x_values = np.arange(1, len(reduced_timeline) + 1, 1)
 
+print(jupiter_rm_vs_time_vs_freq.shape)
 
 x = np.linspace(0, len(jupiter_rm_vs_time)-1, len(jupiter_rm_vs_time))
-plt.plot(x, jupiter_rm_vs_time, 'ro')
+plt.plot(x, jupiter_rm_vs_time_vs_freq[0,:], 'o', color = 'C0', label=f'{freq_bands_str[0]}')
+plt.plot(x, jupiter_rm_vs_time_vs_freq[1,:], 'o', color = 'C1', label=f'{freq_bands_str[1]}')
+plt.plot(x, jupiter_rm_vs_time_vs_freq[2,:], 'o', color = 'C3', label=f'{freq_bands_str[2]}')
+# plt.plot(x, jupiter_rm_vs_time, 'ro')
 # plt.errorbar(x, jupiter_rm_vs_time, yerr = jupiter_rm_std_vs_time) # , fmt ='o'
 plt.xticks(x_values, reduced_timeline, rotation=45)
+plt.legend()
 plt.locator_params(axis='x', nbins=10)
-plt.ylim(1.5, 3.0) 
+plt.ylim(1.5, 20.0) 
 plt.show()
 
 
