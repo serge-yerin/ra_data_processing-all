87d47b3c44c5358c84b1072bf6e4e0ae6a678082
serge-yerin <yerin.serge@gmail.com>
2025-11-26 02:19:19 +0200

    some order added

--- script_rfi_analysis.py
+++ script_rfi_analysis.py
@@ -1,4 +1,4 @@
-
+# Script for RFI analysis in radioastronomical data files in ADR format for Signal project
 
 import os
 import datetime
@@ -9,35 +9,43 @@ from package_ra_data_files_formats.read_file_header_adr import file_header_adr_r
 import matplotlib.pyplot as plt
 from package_ra_data_processing.filtering import median_filter, average_filter  
 
-path_to_data = os.path.normpath('../DATA_preprocessed/')
 
+path_to_data = os.path.normpath('../DATA_preprocessed/')
+f_min = 25.0  # Minimum frequency in MHz
+f_max = 45.0  # Maximum frequency in MHz
+
+
+
+# Get list of .dat files in the specified folder
 dat_file_name_list = find_files_only_in_current_folder(path_to_data, '.dat', 1)
 
-# print(dat_file_name_list)
 
-f_min = 15.0  # Minimum frequency in MHz
-f_max = 25.0  # Maximum frequency in MHz
 
 min_data_array = []
 max_data_array = []
 min_min_data_array = []
 date_array = []
 
-for i in range(len(dat_file_name_list)):
+print('\nReading files in a loop...\n')
+
+# Loop through each .dat file (date of observations)
 # for i in range(3):
-    filename = os.path.join(path_to_data, dat_file_name_list[i])
+for file_index in range(len(dat_file_name_list)):
+
+    # Construct full file path
+    filename = os.path.join(path_to_data, dat_file_name_list[file_index])
     
     with open(filename, 'rb') as file:
         # *** Data file header read ***
         df_filesize = os.stat(filename).st_size                       # Size of file
         df_filename = file.read(32).decode('utf-8').rstrip('\x00')    # Initial data file name
-    
+
     if df_filename[-4:] == '.adr':
         [df_filename, df_filesize, df_system_name, df_obs_place, df_description, CLCfrq, df_creation_timeUTC,
             ReceiverMode, Mode, sumDifMode, NAvr, time_res, fmin, fmax, df, frequency, fft_size, SLine, Width,
             BlockSize] = file_header_adr_read(filename, 0, 0)
 
-        print(f'File: {df_filename}, Size: {df_filesize}, Time: {df_creation_timeUTC}')
+        
 
         file_start_time_utc = datetime.datetime(int('20' + df_filename[1:3]), int(df_filename[3:5]), int(df_filename[5:7]), 
                                                 int(df_creation_timeUTC[0:2]), int(df_creation_timeUTC[3:5]), int(df_creation_timeUTC[6:8]),
@@ -46,13 +54,13 @@ for i in range(len(dat_file_name_list)):
         date_array.append(file_start_time_utc)
 
     else:
-        print(f'File: {df_filename} is not an ADR file, skipping...')
+        print(f'File: {dat_file_name_list[file_index]} is not an ADR file, skipping...')
         continue
     
     num_freq = len(frequency)  # Number of frequency channels
     num_samp = int((df_filesize - 1024) / (num_freq * 8))  # Number of samples in the file
     
-    print(num_freq, num_samp)
+    print(f'File: {df_filename},   Size: {df_filesize},   Time: {df_creation_timeUTC},   Freq: {num_freq},   Samp: {num_samp}')
 
     with open(filename, 'rb') as file:
     
@@ -72,11 +80,10 @@ for i in range(len(dat_file_name_list)):
         max_data_array.append(max_data) 
         min_min_data_array.append(min_min_data)
 
-
-        data = 10 * np.log10(data)
-        min_data = 10 * np.log10(min_data)
-        max_data = 10 * np.log10(max_data)  
-        min_min_data = 10 * np.log10(min_min_data)  
+        with np.errstate(divide='ignore', invalid='ignore'):
+            min_data = 10 * np.log10(min_data)
+            max_data = 10 * np.log10(max_data)  
+            min_min_data = 10 * np.log10(min_min_data)  
 
         # min_data = median_filter(min_data, 25)
         # max_data = median_filter(max_data, 25)
@@ -144,25 +151,25 @@ for i in range(len(dat_file_name_list)):
         # plt.show()
         # plt.close('all')
     
-for i in range(len(date_array)):
-    print(f'File: {dat_file_name_list[i]}, Date: {date_array[i]}')  
+# for i in range(len(date_array)):
+#     print(f'File: {dat_file_name_list[i]}, Date: {date_array[i]}')  
 
 
 min_3_data = np.min(min_min_data_array, axis=0)
 
 
-
-plt.figure(1, figsize=(10.0, 6.0))
-for i in range(len(min_data_array)):
-    plt.plot(frequency, 10 * np.log10(min_data_array[i]), label=f'Min Data {str(date_array[i])[0:19]}')
-plt.plot(frequency, 10 * np.log10(min_3_data))
-plt.title('Min Data Across Files')
-plt.xlabel('Frequency, MHz')
-plt.ylabel('Power (dB)')
-plt.legend()
-plt.tight_layout()
-plt.show()
-plt.close('all')    
+with np.errstate(divide='ignore', invalid='ignore'):
+    plt.figure(1, figsize=(10.0, 6.0))
+    for i in range(len(min_data_array)):
+        plt.plot(frequency, 10 * np.log10(min_data_array[i]), label=f'Min Data {str(date_array[i])[0:19]}')
+    plt.plot(frequency, 10 * np.log10(min_3_data))
+    plt.title('Min Data Across Files')
+    plt.xlabel('Frequency, MHz')
+    plt.ylabel('Power (dB)')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    plt.close('all')    
 
 
 plt.figure(1, figsize=(10.0, 6.0))
@@ -179,11 +186,11 @@ plt.close('all')
 
 min_data_array = np.array(min_data_array)
 
-print(min_data_array.shape)
+# print(min_data_array.shape)
 
 array1 = np.log10(min_data_array[:, range_min_point: range_max_point])
 
-print(array1.shape)
+# print(array1.shape)
 
 
 # array1[3, :] = np.nan  
@@ -208,11 +215,10 @@ plt.imshow(array1, aspect='auto', interpolation='none', origin='lower',
                          cmap='jet')  # extent=[0, num_samp, fmin, fmax]
 # plt.colorbar(label='Power (dB)')
 # plt.title(f'RFI Analysis for {df_filename}')
-
 plt.xticks(x_values, reduced_frequency)
 plt.yticks(y_values, reduced_timeline)
 plt.ylim(y_values[0]-0.5, y_values[-1]+0.5)
-plt.locator_params(axis='x', nbins=int(f_max - f_min)/1)
+plt.locator_params(axis='x', nbins=11)
 plt.ylabel('Date')
 plt.xlabel('Frequency, MHz')
 plt.tight_layout()
